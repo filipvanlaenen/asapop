36904d329968a18568fc45a38f59e023cfe5fb60
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-08-05 17:20:09 +0200

    Issue #42: Continued refactoring export to EOPAOD PSV to separate class

--- src/main/java/net/filipvanlaenen/asapop/exporter/EopaodPsvExporter.java
+++ src/main/java/net/filipvanlaenen/asapop/exporter/EopaodPsvExporter.java
@@ -1,6 +1,8 @@
 package net.filipvanlaenen.asapop.exporter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import net.filipvanlaenen.asapop.model.OpinionPoll;
 import net.filipvanlaenen.asapop.model.OpinionPolls;
@@ -38,49 +40,20 @@ public class EopaodPsvExporter {
      * @return A string containing the opinion poll in the PSV file format for EOPAOD.
      */
     static String export(final OpinionPoll opinionPoll, final String... electoralListKeys) {
-        StringBuffer sb = new StringBuffer();
-        sb.append(opinionPoll.getPollingFirm());
-        sb.append(" | ");
-        sb.append(opinionPoll.getCommissioners().isEmpty() ? "N/A" : String.join(", ", opinionPoll.getCommissioners()));
-        sb.append(" | ");
-        LocalDate fieldworkStart = opinionPoll.getFieldworkStart();
-        LocalDate fieldworkEnd = opinionPoll.getFieldworkEnd();
-        LocalDate publicationDate = opinionPoll.getPublicationDate();
-        if (fieldworkStart == null) {
-            if (fieldworkEnd == null) {
-                sb.append(publicationDate.toString());
-                sb.append(" | ");
-                sb.append(publicationDate.toString());
-            } else {
-                sb.append(fieldworkEnd.toString());
-                sb.append(" | ");
-                sb.append(fieldworkEnd.toString());
-            }
-        } else {
-            if (fieldworkEnd == null) {
-                sb.append(fieldworkStart.toString());
-                sb.append(" | ");
-                sb.append(publicationDate.toString());
-            } else {
-                sb.append(fieldworkStart.toString());
-                sb.append(" | ");
-                sb.append(fieldworkEnd.toString());
-            }
-        }
-        sb.append(" | ");
-        sb.append(naIfNull(opinionPoll.getScope()));
-        sb.append(" | ");
-        sb.append(naIfNull(opinionPoll.getSampleSize()));
-        sb.append(" | ");
-        sb.append("N/A");
-        sb.append(" | ");
-        sb.append("N/A");
-        sb.append(" | ");
+        List<String> elements = new ArrayList<String>();
+        elements.add(opinionPoll.getPollingFirm());
+        elements.add(opinionPoll.getCommissioners().isEmpty() ? "N/A" : String.join(", ", opinionPoll.getCommissioners()));
+        elements.addAll(exportDates(opinionPoll));
+        elements.add(naIfNull(opinionPoll.getScope()));
+        elements.add(naIfNull(opinionPoll.getSampleSize()));
+        elements.add("N/A");
+        elements.add("N/A");
         for (String electoralListKey : electoralListKeys) {
-            sb.append(naIfNull(opinionPoll.getResult(electoralListKey)));
-            sb.append(" | ");
+            elements.add(naIfNull(opinionPoll.getResult(electoralListKey)));
         }
-        sb.append(naIfNull(opinionPoll.getOther()));
+        elements.add(naIfNull(opinionPoll.getOther()));
+        StringBuffer sb = new StringBuffer();
+        sb.append(String.join(" | ", elements));
         for (ResponseScenario responseScenario : opinionPoll.getAlternativeResponseScenarios()) {
             sb.append("\n");
             sb.append(export(responseScenario, opinionPoll, electoralListKeys));
@@ -96,52 +69,58 @@ public class EopaodPsvExporter {
      * @return A string containing the response scenario in the PSV file format for EOPAOD.
      */
     static String export(final ResponseScenario responseScenario, final OpinionPoll opinionPoll, final String... electoralListKeys) {
-        StringBuffer sb = new StringBuffer();
-        sb.append(opinionPoll.getPollingFirm());
-        sb.append(" | ");
-        sb.append(opinionPoll.getCommissioners().isEmpty() ? "N/A" : String.join(", ", opinionPoll.getCommissioners()));
-        sb.append(" | ");
+        List<String> elements = new ArrayList<String>();
+        elements.add(opinionPoll.getPollingFirm());
+        elements.add(opinionPoll.getCommissioners().isEmpty() ? "N/A" : String.join(", ", opinionPoll.getCommissioners()));
+        elements.addAll(exportDates(opinionPoll));
+        elements.add(responseScenario.getScope() == null ? naIfNull(opinionPoll.getScope()) : responseScenario.getScope());
+        elements.add(naIfNull(opinionPoll.getSampleSize()));
+        elements.add("N/A");
+        elements.add("N/A");
+        for (String electoralListKey : electoralListKeys) {
+            elements.add(naIfNull(responseScenario.getResult(electoralListKey)));
+        }
+        elements.add(naIfNull(responseScenario.getOther()));
+        return String.join(" | ", elements);
+    }
+
+    /**
+     * Exports the dates from the opinion poll to two data elements.
+     *
+     * @param opinionPoll The opinion poll to export the dates from.
+     * @return A list with two elements containing the dates for the EOPAOD PSV file.
+     */
+    private static List<String> exportDates(final OpinionPoll opinionPoll) {
+        List<String> elements = new ArrayList<String>();
         LocalDate fieldworkStart = opinionPoll.getFieldworkStart();
         LocalDate fieldworkEnd = opinionPoll.getFieldworkEnd();
         LocalDate publicationDate = opinionPoll.getPublicationDate();
-        if (opinionPoll.getFieldworkStart() == null) {
-            if (opinionPoll.getFieldworkEnd() == null) {
-                sb.append(opinionPoll.getPublicationDate().toString());
-                sb.append(" | ");
-                sb.append(opinionPoll.getPublicationDate().toString());
+        if (fieldworkStart == null) {
+            if (fieldworkEnd == null) {
+                elements.add(publicationDate.toString());
+                elements.add(publicationDate.toString());
             } else {
-                sb.append(opinionPoll.getFieldworkEnd().toString());
-                sb.append(" | ");
-                sb.append(opinionPoll.getFieldworkEnd().toString());
+                elements.add(fieldworkEnd.toString());
+                elements.add(fieldworkEnd.toString());
             }
         } else {
-            if (opinionPoll.getFieldworkEnd() == null) {
-                sb.append(opinionPoll.getFieldworkStart().toString());
-                sb.append(" | ");
-                sb.append(opinionPoll.getPublicationDate().toString());
+            if (fieldworkEnd == null) {
+                elements.add(fieldworkStart.toString());
+                elements.add(publicationDate.toString());
             } else {
-                sb.append(opinionPoll.getFieldworkStart().toString());
-                sb.append(" | ");
-                sb.append(opinionPoll.getFieldworkEnd().toString());
+                elements.add(fieldworkStart.toString());
+                elements.add(fieldworkEnd.toString());
             }
         }
-        sb.append(" | ");
-        sb.append(responseScenario.getScope() == null ? naIfNull(opinionPoll.getScope()) : responseScenario.getScope());
-        sb.append(" | ");
-        sb.append(naIfNull(opinionPoll.getSampleSize()));
-        sb.append(" | ");
-        sb.append("N/A");
-        sb.append(" | ");
-        sb.append("N/A");
-        sb.append(" | ");
-        for (String electoralListKey : electoralListKeys) {
-            sb.append(naIfNull(responseScenario.getResult(electoralListKey)));
-            sb.append(" | ");
-        }
-        sb.append(naIfNull(responseScenario.getOther()));
-        return sb.toString();
+        return elements;
     }
 
+    /**
+     * Returns the string if it isn't null, and the string "N/A" otherwise.
+     *
+     * @param s The string.
+     * @return "N/A" if the string is null, and otherwise the string as provided.
+     */
     private static String naIfNull(final String s) {
         return s == null ? "N/A" : s;
     }
