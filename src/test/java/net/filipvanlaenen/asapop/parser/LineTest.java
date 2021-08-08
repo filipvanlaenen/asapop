59dc16c568e72e4e0a4aa3c6fb276715e498a489
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-08-08 17:54:45 +0200

    Issue #43: Added unit tests to Line

---
+++ src/test/java/net/filipvanlaenen/asapop/parser/LineTest.java
@@ -0,0 +1,27 @@
+package net.filipvanlaenen.asapop.parser;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests on the <code>Line</code> class.
+ */
+public final class LineTest {
+    /**
+     * Verifies that a match is detected by the <code>lineMatchesPattern</code> method.
+     */
+    @Test
+    public void lineMatchesPatternShouldDetectAMatch() {
+        assertTrue(Line.lineMatchesPattern(Line.RESULT_KEY_VALUE_PATTERN, "A:5"));
+    }
+
+    /**
+     * Verifies that a non-match is detected by the <code>lineMatchesPattern</code> method.
+     */
+    @Test
+    public void lineMatchesPatternShouldDetectANonMatch() {
+        assertFalse(Line.lineMatchesPattern(Line.RESULT_KEY_VALUE_PATTERN, "a:5"));
+    }
+}
