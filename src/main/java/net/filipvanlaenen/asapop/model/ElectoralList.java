05ddb5a70af9bdc29a3db8d2cf81349ccbd7d26c
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-08-14 18:43:42 +0200

    Issue #17: Resolved PMD violations

--- src/main/java/net/filipvanlaenen/asapop/model/ElectoralList.java
+++ src/main/java/net/filipvanlaenen/asapop/model/ElectoralList.java
@@ -1,6 +1,5 @@
 package net.filipvanlaenen.asapop.model;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
--- src/main/java/net/filipvanlaenen/asapop/parser/RichOpinionPollsFile.java
+++ src/main/java/net/filipvanlaenen/asapop/parser/RichOpinionPollsFile.java
@@ -3,7 +3,6 @@ package net.filipvanlaenen.asapop.parser;
 import java.util.HashSet;
 import java.util.Set;
 
-import net.filipvanlaenen.asapop.model.ElectoralList;
 import net.filipvanlaenen.asapop.model.OpinionPoll;
 import net.filipvanlaenen.asapop.model.OpinionPolls;
 
