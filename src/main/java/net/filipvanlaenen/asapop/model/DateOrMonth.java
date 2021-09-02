0ebaa804e29625f34ab643248fd31ca0482d5146
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-09-02 21:51:19 +0200

    Issue #37: Added some unit tests

--- src/main/java/net/filipvanlaenen/asapop/model/DateOrMonth.java
+++ src/main/java/net/filipvanlaenen/asapop/model/DateOrMonth.java
@@ -6,14 +6,27 @@ import java.time.YearMonth;
 import java.util.Objects;
 
 /**
- * Class wrapping around either a Date (<code>LocalDate</code>) or a month (<code>YearMonth</code>).
+ * Class wrapping around either a date (<code>LocalDate</code>) or a month (<code>YearMonth</code>).
  */
 public abstract class DateOrMonth {
+    /**
+     * Subclass wrapping around a date.
+     */
     private static final class Date extends DateOrMonth {
+        /**
+         * The date.
+         */
         private LocalDate date;
+
+        /**
+         * Constructor taking the date as its parameter.
+         *
+         * @param date The date to wrap around.
+         */
         private Date(final LocalDate date) {
             this.date = date;
         }
+
         @Override
         public boolean equals(final Object obj) {
             if (obj instanceof Date) {
@@ -23,26 +36,56 @@ public abstract class DateOrMonth {
                 return false;
             }
         }
+
+        /**
+         * Returns the end date, which is identical to the date.
+         *
+         * @return The end date.
+         */
+        @Override
         public LocalDate getEnd() {
             return date;
         }
+
+        /**
+         * Returns the start date, which is identical to the date.
+         *
+         * @return The start date.
+         */
+        @Override
         public LocalDate getStart() {
             return date;
         }
+
         @Override
         public int hashCode() {
             return Objects.hash(date);
         }
+
         @Override
         public String toString() {
             return date.toString();
         }
     }
+
+    /**
+     * Subclass wrapping around a month.
+     */
     private static final class Month extends DateOrMonth {
+        /**
+         * The month.
+         */
         private YearMonth month;
+
+        /**
+         * Constructor taking the month as its parameter.
+         *
+         * @param month The month to wrap around.
+         */
         private Month(final YearMonth month) {
             this.month = month;
         }
+
         @Override
         public boolean equals(final Object obj) {
             if (obj instanceof Month) {
@@ -52,21 +95,44 @@ public abstract class DateOrMonth {
                 return false;
             }
         }
+
+        /**
+         * Returns the end date, which is the last day of the month.
+         *
+         * @return The end date.
+         */
+        @Override
         public LocalDate getEnd() {
             return month.atEndOfMonth();
         }
+
+        /**
+         * Returns the start date, which is the first day of the month.
+         *
+         * @return The start date.
+         */
+        @Override
         public LocalDate getStart() {
             return month.atDay​(1);
         }
+
         @Override
         public int hashCode() {
             return Objects.hash(month);
         }
+
         @Override
         public String toString() {
             return month.toString();
         }
     }
+
+    /**
+     * Parses a text into a date or month. It tries to parse the text into a date first, and then a month.
+     *
+     * @param text The text to parse.
+     * @return A date or month wrapped in a DateOrMonth instance.
+     */
     static DateOrMonth parse(final String text) {
         try {
             return new Date(LocalDate.parse(text));
@@ -74,6 +140,18 @@ public abstract class DateOrMonth {
             return new Month(YearMonth.parse(text));
         }
     }
+
+    /**
+     * Returns the end of the date or month.
+     *
+     * @return The end of the date or month.
+     */
     public abstract LocalDate getEnd();
+
+    /**
+     * Returns the start of the date or month.
+     *
+     * @return The start of the date or month.
+     */
     public abstract LocalDate getStart();
 }
--- src/test/java/net/filipvanlaenen/asapop/model/DateOrMonthTest.java
+++ src/test/java/net/filipvanlaenen/asapop/model/DateOrMonthTest.java
@@ -14,12 +14,25 @@ import org.junit.jupiter.api.Test;
  */
 public class DateOrMonthTest {
     /**
+     * 2 September 2021 as a text.
+     */
+    private static final String SECOND_OF_SEPTEMBER_2021_TEXT = "2021-09-02";
+    /**
+     * September 2021 as a text.
+     */
+    private static final String SEPTEMBER_2021_TEXT = "2021-09";
+    /**
+     * 2 September 2021 as a date.
+     */
+    private static final LocalDate SECOND_OF_SEPTEMBER_2021_DATE = LocalDate.of(2021, 9, 2);
+
+    /**
      * Verifies that for a date, the start is the date itself.
      */
      @Test
      public void getStartShouldReturnTheDateItself() {
-         DateOrMonth date = DateOrMonth.parse("2021-09-02");
-         assertEquals(LocalDate.parse("2021-09-02"), date.getStart());
+         DateOrMonth date = DateOrMonth.parse(SECOND_OF_SEPTEMBER_2021_TEXT);
+         assertEquals(SECOND_OF_SEPTEMBER_2021_DATE, date.getStart());
     }
 
     /**
@@ -27,7 +40,7 @@ public class DateOrMonthTest {
      */
      @Test
      public void getStartShouldReturnTheFirstDayOfTheMonth() {
-         DateOrMonth date = DateOrMonth.parse("2021-09");
+         DateOrMonth date = DateOrMonth.parse(SEPTEMBER_2021_TEXT);
          assertEquals(LocalDate.parse("2021-09-01"), date.getStart());
     }
 
@@ -36,8 +49,8 @@ public class DateOrMonthTest {
      */
      @Test
      public void getEndShouldReturnTheDateItself() {
-         DateOrMonth date = DateOrMonth.parse("2021-09-02");
-         assertEquals(LocalDate.parse("2021-09-02"), date.getEnd());
+         DateOrMonth date = DateOrMonth.parse(SECOND_OF_SEPTEMBER_2021_TEXT);
+         assertEquals(SECOND_OF_SEPTEMBER_2021_DATE, date.getEnd());
     }
 
     /**
@@ -45,7 +58,158 @@ public class DateOrMonthTest {
      */
      @Test
      public void getEndShouldReturnTheLastDayOfTheMonth() {
-         DateOrMonth date = DateOrMonth.parse("2021-09");
+         DateOrMonth date = DateOrMonth.parse(SEPTEMBER_2021_TEXT);
          assertEquals(LocalDate.parse("2021-09-30"), date.getEnd());
+    }
+
+    /**
+     * Verifies that a date is converted correctly to a string.
+     */
+    @Test
+    public void shouldConvertADateToAStringCorrectly() {
+        assertEquals(SECOND_OF_SEPTEMBER_2021_TEXT, DateOrMonth.parse(SECOND_OF_SEPTEMBER_2021_TEXT).toString());
+    }
+
+    /**
+     * Verifies that a month is converted correctly to a string.
+     */
+    @Test
+    public void shouldConvertAMonthToAStringCorrectly() {
+        assertEquals(SEPTEMBER_2021_TEXT, DateOrMonth.parse(SEPTEMBER_2021_TEXT).toString());
+    }
+
+    /**
+     * Verifies that a date is not equal to null.
+     */
+    @Test
+    public void aDateShouldNotBeEqualToNull() {
+        assertFalse(DateOrMonth.parse(SECOND_OF_SEPTEMBER_2021_TEXT).equals(null));
+    }
+
+    /**
+     * Verifies that a month is not equal to null.
+     */
+    @Test
+    public void aMonthShouldNotBeEqualToNull() {
+        assertFalse(DateOrMonth.parse(SEPTEMBER_2021_TEXT).equals(null));
+    }
+
+    /**
+     * Verifies that a date is not equal to an object of another class, like a string.
+     */
+    @Test
+    public void aDateShouldNotBeEqualToAString() {
+        assertFalse(DateOrMonth.parse(SECOND_OF_SEPTEMBER_2021_TEXT).equals(""));
+    }
+
+    /**
+     * Verifies that a month is not equal to an object of another class, like a string.
+     */
+    @Test
+    public void aMonthShouldNotBeEqualToAString() {
+        assertFalse(DateOrMonth.parse(SEPTEMBER_2021_TEXT).equals(""));
+    }
+
+    /**
+     * Verifies that a date is equal to itself.
+     */
+    @Test
+    public void aDateShouldBeEqualToItself() {
+        DateOrMonth date = DateOrMonth.parse(SECOND_OF_SEPTEMBER_2021_TEXT);
+        assertTrue(date.equals(date));
+    }
+
+    /**
+     * Verifies that a month is equal to itself.
+     */
+    @Test
+    public void aMonthShouldBeEqualToItself() {
+        DateOrMonth month = DateOrMonth.parse(SEPTEMBER_2021_TEXT);
+        assertTrue(month.equals(month));
+    }
+
+    /**
+     * Verifies that calling hashCode twice on a date returns the same result.
+     */
+    @Test
+    public void callingHashCodeTwiceOnDateReturnsTheSameResult() {
+        DateOrMonth date = DateOrMonth.parse(SECOND_OF_SEPTEMBER_2021_TEXT);
+        assertEquals(date.hashCode(), date.hashCode());
+    }
+
+    /**
+     * Verifies that calling hashCode twice on a month returns the same result.
+     */
+    @Test
+    public void callingHashCodeTwiceOnMonthReturnsTheSameResult() {
+        DateOrMonth month = DateOrMonth.parse(SEPTEMBER_2021_TEXT);
+        assertEquals(month.hashCode(), month.hashCode());
+    }
+
+    /**
+     * Verifies that two dates parsed from the same text are equal.
+     */
+    @Test
+    public void twoDatesParsedFromTheSameStringShouldBeEqual() {
+        assertEquals(DateOrMonth.parse(SECOND_OF_SEPTEMBER_2021_TEXT),
+                     DateOrMonth.parse(SECOND_OF_SEPTEMBER_2021_TEXT));
+    }
+
+    /**
+     * Verifies that two months parsed from the same text are equal.
+     */
+    @Test
+    public void twoMonthsParsedFromTheSameStringShouldBeEqual() {
+        assertEquals(DateOrMonth.parse(SEPTEMBER_2021_TEXT), DateOrMonth.parse(SEPTEMBER_2021_TEXT));
+    }
+
+    /**
+     * Verifies that two dates parsed from the same text return the same hashCode.
+     */
+    @Test
+    public void twoDatesParsedFromTheSameStringShouldHaveTheSameHashCode() {
+        assertEquals(DateOrMonth.parse(SECOND_OF_SEPTEMBER_2021_TEXT).hashCode(),
+                     DateOrMonth.parse(SECOND_OF_SEPTEMBER_2021_TEXT).hashCode());
+    }
+
+    /**
+     * Verifies that two months parsed from the same text return the same hashCode.
+     */
+    @Test
+    public void twoMonthsParsedFromTheSameStringShouldHaveTheSameHashCode() {
+        assertEquals(DateOrMonth.parse(SEPTEMBER_2021_TEXT).hashCode(),
+                     DateOrMonth.parse(SEPTEMBER_2021_TEXT).hashCode());
+    }
+
+    /**
+     * Verifies that two different dates are not equal.
+     */
+    @Test
+    public void twoDifferentDatesShouldNotBeEqual() {
+        assertFalse(DateOrMonth.parse(SECOND_OF_SEPTEMBER_2021_TEXT).equals(DateOrMonth.parse("2021-10-02")));
+    }
+
+    /**
+     * Verifies that two different months are not equal.
+     */
+    @Test
+    public void twoDifferentMonthsShouldNotBeEqual() {
+        assertFalse(DateOrMonth.parse(SEPTEMBER_2021_TEXT).equals(DateOrMonth.parse("2021-10")));
+    }
+
+    /**
+     * Verifies that two different dates have different hashCodes.
+     */
+    @Test
+    public void twoDifferentDatesShouldHaveDifferentHashCodes() {
+        assertFalse(DateOrMonth.parse(SECOND_OF_SEPTEMBER_2021_TEXT).hashCode() == DateOrMonth.parse("2021-10-02").hashCode());
+    }
+
+    /**
+     * Verifies that two different months have different hashCodes.
+     */
+    @Test
+    public void twoDifferentMonthsShouldHaveDifferentHashCodes() {
+        assertFalse(DateOrMonth.parse(SEPTEMBER_2021_TEXT).hashCode() == DateOrMonth.parse("2021-10").hashCode());
     }
 }
