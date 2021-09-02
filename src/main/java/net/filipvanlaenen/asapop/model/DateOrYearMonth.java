a0c577ecbd2dade63b0042ad6c1b82e3df0dfae7
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-09-02 18:24:20 +0200

    Issue #37: Added functionality to handle months

--- src/main/java/net/filipvanlaenen/asapop/model/DateOrYearMonth.java
+++ src/main/java/net/filipvanlaenen/asapop/model/DateOrYearMonth.java
@@ -1,40 +1,79 @@
 package net.filipvanlaenen.asapop.model;
 
+import java.time.format.DateTimeParseException;
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.util.Objects;
 
 /**
  * Class wrapping around either a Date (<code>LocalDate</code>) or <code>YearMonth</code>.
  */
-public final class DateOrYearMonth {
-    private LocalDate date;
-    private DateOrYearMonth(final LocalDate date) {
-        this.date = date;
-    }
-    static DateOrYearMonth parse(final String text) {
-        return new DateOrYearMonth(LocalDate.parse(text));
-    }
-    @Override
-    public boolean equals(final Object obj) {
-        if (obj instanceof DateOrYearMonth) {
-            DateOrYearMonth otherDateOrYearMonth = (DateOrYearMonth) obj;
-            return otherDateOrYearMonth.date.equals(date);
-        } else {
-            return false;
+public abstract class DateOrYearMonth {
+    private static final class Date extends DateOrYearMonth {
+        private LocalDate date;
+        private Date(final LocalDate date) {
+            this.date = date;
+        }
+        @Override
+        public boolean equals(final Object obj) {
+            if (obj instanceof Date) {
+                Date otherDate = (Date) obj;
+                return otherDate.date.equals(date);
+            } else {
+                return false;
+            }
+        }
+        public LocalDate getEnd() {
+            return date;
+        }
+        public LocalDate getStart() {
+            return date;
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(date);
+        }
+        @Override
+        public String toString() {
+            return date.toString();
         }
     }
-    public LocalDate getEnd() {
-        return date;
+    private static final class Month extends DateOrYearMonth {
+        private YearMonth month;
+        private Month(final YearMonth month) {
+            this.month = month;
+        }
+        @Override
+        public boolean equals(final Object obj) {
+            if (obj instanceof Month) {
+                Month otherMonth = (Month) obj;
+                return otherMonth.month.equals(month);
+            } else {
+                return false;
+            }
+        }
+        public LocalDate getEnd() {
+            return month.atEndOfMonth();
+        }
+        public LocalDate getStart() {
+            return month.atDay​(1);
+        }
+        @Override
+        public int hashCode() {
+            return Objects.hash(month);
+        }
+        @Override
+        public String toString() {
+            return month.toString();
+        }
     }
-    public LocalDate getStart() {
-        return date;
+    static DateOrYearMonth parse(final String text) {
+        try {
+            return new Date(LocalDate.parse(text));
+        } catch (DateTimeParseException dtpe) {
+            return new Month(YearMonth.parse(text));
+        }
     }
-    @Override
-    public int hashCode() {
-        return Objects.hash(date);
-    }
-    @Override
-    public String toString() {
-        return date.toString();
-    }
+    public abstract LocalDate getEnd();
+    public abstract LocalDate getStart();
 }
---
+++ src/test/java/net/filipvanlaenen/asapop/model/DateOrYearMonthTest.java
@@ -0,0 +1,13 @@
+package net.filipvanlaenen.asapop.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests on the class <code>DateOrYearMonth</code>.
+ */
+public class DateOrYearMonthTest {
+}
