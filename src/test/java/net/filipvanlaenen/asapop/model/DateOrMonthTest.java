1178dec90c3f98848e7b1a460c36ca49adee6a84
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-09-02 21:55:26 +0200

    Issue #37: Resolved a PMD violation

--- src/test/java/net/filipvanlaenen/asapop/model/DateOrMonthTest.java
+++ src/test/java/net/filipvanlaenen/asapop/model/DateOrMonthTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalDate;
-import java.time.YearMonth;
 
 import org.junit.jupiter.api.Test;
 
