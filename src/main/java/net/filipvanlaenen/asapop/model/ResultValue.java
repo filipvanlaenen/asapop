487211ea8fb16216327bec1d5c5023250fb2656d
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-11-13 21:01:14 +0100

    Issue #21: Resolved Checkstyle issues

--- src/main/java/net/filipvanlaenen/asapop/model/ResultValue.java
+++ src/main/java/net/filipvanlaenen/asapop/model/ResultValue.java
@@ -2,14 +2,33 @@ package net.filipvanlaenen.asapop.model;
 
 import java.util.Objects;
 
-public class ResultValue {
+/**
+ * Class modeling a result value.
+ */
+public final class ResultValue {
+    /**
+     * The text representing the result value.
+     */
     private final String text;
+
+    /**
+     * Constructor taking the text representing the result value as its parameter.
+     *
+     * @param text The text representing the result value.
+     */
     public ResultValue(final String text) {
         this.text = text;
     }
+
+    /**
+     * Returns the text representing the result value.
+     *
+     * @return The text representing the result value.
+     */
     public String getText() {
         return text;
     }
+
     @Override
     public boolean equals(final Object obj) {
         if (obj instanceof ResultValue) {
@@ -19,6 +38,7 @@ public class ResultValue {
             return false;
         }
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(text);
