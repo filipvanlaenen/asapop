f37a9aee7bf81c356a5552c541f9cecbf1e92f6a
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-11-13 12:03:51 +0100

    Issue #21: Added unit tests to MalformedResultValueWarning

--- src/test/java/net/filipvanlaenen/asapop/parser/MalformedResultValueWarningTest.java
+++ src/test/java/net/filipvanlaenen/asapop/parser/MalformedResultValueWarningTest.java
@@ -1,6 +1,8 @@
 package net.filipvanlaenen.asapop.parser;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -15,5 +17,57 @@ public final class MalformedResultValueWarningTest {
     public void toStringShouldProduceCorrectMessage() {
         Warning warning = new MalformedResultValueWarning(1, "a");
         assertEquals("Malformed result value (“a”) detected in line 1.", warning.toString());
+    }
+
+    /**
+     * Verifies that a malformed result value warning is not equal to null.
+     */
+    @Test
+    public void aMalformedResultValueWarningShouldNotBeEqualToNull() {
+        assertFalse(new MalformedResultValueWarning(1, "a").equals(null));
+    }
+
+    /**
+     * Verifies that a malformed result value warning is not equal to an object of another class, like a string.
+     */
+    @Test
+    public void aMalformedResultValueWarningShouldNotBeEqualToAString() {
+        assertFalse(new MalformedResultValueWarning(1, "a").equals(""));
+    }
+
+    /**
+     * Verifies that a malformed result value warning is equal to itself.
+     */
+    @Test
+    public void aMalformedResultValueWarningShouldBeEqualToItself() {
+        MalformedResultValueWarning warning = new MalformedResultValueWarning(1, "a");
+        assertTrue(warning.equals(warning));
+    }
+
+    /**
+     * Verifies that calling hashCode twice on a malformed result value warning returns the same result.
+     */
+    @Test
+    public void callingHashCodeTwiceOnMalformedResultValueWarningReturnsTheSameResult() {
+        MalformedResultValueWarning warning = new MalformedResultValueWarning(1, "a");
+        assertEquals(warning.hashCode(), warning.hashCode());
+    }
+
+    /**
+     * Verifies that two malformed result value warnings constructed with the same parameters are equal.
+     */
+    @Test
+    public void twoMalformedResultValueWarningsConstructedWithTheSameParametersShouldBeEqual() {
+        assertEquals(new MalformedResultValueWarning(1, "a"),
+                     new MalformedResultValueWarning(1, "a"));
+    }
+
+    /**
+     * Verifies that two malformed result value warnings constructed with the same parameters return the same hashCode.
+     */
+    @Test
+    public void twoMalformedResultValueWarningsConstructedWithTheSameParametersShouldHaveTheSameHashCode() {
+        assertEquals(new MalformedResultValueWarning(1, "a").hashCode(),
+                     new MalformedResultValueWarning(1, "a").hashCode());
     }
 }
