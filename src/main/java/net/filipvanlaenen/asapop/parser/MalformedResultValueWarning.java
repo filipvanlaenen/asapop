288059427382c7ff9524500cd7844a0991657f84
Filip van Laenen <f.a.vanlaenen@ieee.org>
2021-11-13 20:55:30 +0100

    Issue #21: Resolved Checkstyle issues

--- src/main/java/net/filipvanlaenen/asapop/parser/MalformedResultValueWarning.java
+++ src/main/java/net/filipvanlaenen/asapop/parser/MalformedResultValueWarning.java
@@ -2,13 +2,30 @@ package net.filipvanlaenen.asapop.parser;
 
 import java.util.Objects;
 
+/**
+ * A warning about a malformed result value.
+ */
 class MalformedResultValueWarning implements Warning {
+    /**
+     * The number of the line where the warning occured.
+     */
     private final int lineNumber;
+    /**
+     * The malformed value.
+     */
     private final String value;
+
+    /**
+     * Constructor taking the line number and the value as its parameters.
+     *
+     * @param lineNumber The number of the line where the warning occured.
+     * @param value The malformed value.
+     */
     MalformedResultValueWarning(final int lineNumber, final String value) {
         this.lineNumber = lineNumber;
         this.value = value;
     }
+
     @Override
     public boolean equals(final Object obj) {
         if (obj instanceof MalformedResultValueWarning) {
@@ -18,12 +35,14 @@ class MalformedResultValueWarning implements Warning {
             return false;
         }
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(lineNumber, value);
     }
+
     @Override
     public String toString() {
-        return "Malformed result value (“" + value + "”) detected in line " + lineNumber+".";
+        return "Malformed result value (“" + value + "”) detected in line " + lineNumber + ".";
     }
 }
