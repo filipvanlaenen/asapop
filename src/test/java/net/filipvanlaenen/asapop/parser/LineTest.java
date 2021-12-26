0b78f86821ed6cc3340faf5048f5a25d58e31c20
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2021-12-26 23:17:47 +0100

    Issue #76: Added a unit test to Line

--- src/test/java/net/filipvanlaenen/asapop/parser/LineTest.java
+++ src/test/java/net/filipvanlaenen/asapop/parser/LineTest.java
@@ -1,9 +1,13 @@
 package net.filipvanlaenen.asapop.parser;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
+
+import net.filipvanlaenen.asapop.model.Scope;
 
 /**
  * Unit tests on the <code>Line</code> class.
@@ -23,5 +27,21 @@ public final class LineTest {
     @Test
     public void lineMatchesPatternShouldDetectANonMatch() {
         assertFalse(Line.lineMatchesPattern(Line.RESULT_KEY_VALUE_PATTERN, "a:5"));
+    }
+
+    /**
+     * Verifies that the parser method for the scopes returns a value for string E.
+     */
+    @Test
+    public void parseScopeShouldReturnAScopeForAValidValue() {
+        assertEquals(Scope.European, Line.parseScope("E"));
+    }
+
+    /**
+     * Verifies that the parser method for the scopes returns <code>null</code> for an invalid string.
+     */
+    @Test
+    public void parseScopeShouldReturnNullForInvalidValue() {
+        assertNull(Line.parseScope("X"));
     }
 }
