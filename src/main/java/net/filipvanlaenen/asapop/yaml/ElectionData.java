42069df0eb1b65c2a14a39272787eb559008817a
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-01-01 20:17:15 +0100

    Issue #77: Resolved Checkstyle violations for ElectionData

--- src/main/java/net/filipvanlaenen/asapop/yaml/ElectionData.java
+++ src/main/java/net/filipvanlaenen/asapop/yaml/ElectionData.java
@@ -1,4 +1,8 @@
 package net.filipvanlaenen.asapop.yaml;
 
+/**
+ * Class representing the top element for the YAML file containing the election data needed for the analysis of the
+ * opinion polls in an ROPF file.
+ */
 public class ElectionData {
 }
