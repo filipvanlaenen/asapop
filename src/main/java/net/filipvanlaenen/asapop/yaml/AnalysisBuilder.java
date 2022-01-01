9507a52241f4eace57d7419f91638dbb4683df9f
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-01-01 20:58:17 +0100

    Issue #77: Resolved Checkstyle violations for AnalysisBuilder

--- src/main/java/net/filipvanlaenen/asapop/yaml/AnalysisBuilder.java
+++ src/main/java/net/filipvanlaenen/asapop/yaml/AnalysisBuilder.java
@@ -12,14 +12,33 @@ import net.filipvanlaenen.asapop.model.ElectoralList;
 import net.filipvanlaenen.asapop.model.OpinionPoll;
 import net.filipvanlaenen.asapop.model.ResponseScenario;
 
+/**
+ * Builder class to build an <code>Analysis</code>.
+ */
 public class AnalysisBuilder {
+    /**
+     * The levels for the confidence intervals.
+     */
     private static final Integer[] CONFIDENCE_INTERVAL_LEVELS = new Integer[] {80, 90, 95, 99};
+    /**
+     * The analysis engine to extract the analysis from.
+     */
     private AnalysisEngine engine;
 
-    public AnalysisBuilder(AnalysisEngine engine) {
+    /**
+     * Constructor taking an analysis engine as its parameter.
+     *
+     * @param engine The analysis engine to extract the analysis from.
+     */
+    public AnalysisBuilder(final AnalysisEngine engine) {
         this.engine = engine;
     }
 
+    /**
+     * Builds the analysis.
+     *
+     * @return The analysis extracted from the analysis engine.
+     */
     public Analysis build() {
         Analysis result = new Analysis();
         Set<OpinionPollAnalysis> opinionPollAnalyses = new HashSet<OpinionPollAnalysis>();
@@ -30,7 +49,13 @@ public class AnalysisBuilder {
         return result;
     }
 
-    private OpinionPollAnalysis buildOpinionPollAnalysis(OpinionPoll poll) {
+    /**
+     * Builds an opinion poll analysis for an opinion poll.
+     *
+     * @param poll The opinion poll for which to build an opinion poll analysis.
+     * @return Returns the opinion poll analysis for the opinion poll.
+     */
+    private OpinionPollAnalysis buildOpinionPollAnalysis(final OpinionPoll poll) {
         OpinionPollAnalysis opinionPollAnalysis = new OpinionPollAnalysis();
         opinionPollAnalysis.setPollingFirm(poll.getPollingFirm());
         opinionPollAnalysis.setPollingFirmPartner(poll.getPollingFirmPartner());
@@ -47,7 +72,13 @@ public class AnalysisBuilder {
         return opinionPollAnalysis;
     }
 
-    private ResultAnalysis buildOtherAnalysis(ResponseScenario responseScenario) {
+    /**
+     * Builds the result analysis for the other result in a response scenario.
+     *
+     * @param responseScenario The response scenario.
+     * @return A result analysis for other in a response scenario.
+     */
+    private ResultAnalysis buildOtherAnalysis(final ResponseScenario responseScenario) {
         ResultAnalysis resultAnalysis = new ResultAnalysis();
         resultAnalysis.setMedian(responseScenario.getMedianOther());
         Map<Integer, Float[]> confidenceIntervals = new HashMap<Integer, Float[]>();
@@ -58,7 +89,13 @@ public class AnalysisBuilder {
         return resultAnalysis;
     }
 
-    private ResponseScenarioAnalysis buildResponseScenarioAnalysis(OpinionPoll poll) {
+    /**
+     * Builds the response scenario analysis for an opinion poll's main response scenario.
+     *
+     * @param poll The opinion poll for which to build the response scenario analysis.
+     * @return A response scenario analysis for an opinion poll's main response scenario.
+     */
+    private ResponseScenarioAnalysis buildResponseScenarioAnalysis(final OpinionPoll poll) {
         ResponseScenarioAnalysis responseScenarioAnalysis = new ResponseScenarioAnalysis();
         responseScenarioAnalysis.setArea(poll.getArea());
         responseScenarioAnalysis.setScope(nullOrToString(poll.getScope()));
@@ -72,7 +109,13 @@ public class AnalysisBuilder {
         return responseScenarioAnalysis;
     }
 
-    private ResponseScenarioAnalysis buildResponseScenarioAnalysis(ResponseScenario responseScenario) {
+    /**
+     * Builds the response scenario analysis for a response scenario.
+     *
+     * @param responseScenario The response scenario for which to build the response scenario analysis.
+     * @return The response scenarion analysis for the response scenario.
+     */
+    private ResponseScenarioAnalysis buildResponseScenarioAnalysis(final ResponseScenario responseScenario) {
         ResponseScenarioAnalysis responseScenarioAnalysis = new ResponseScenarioAnalysis();
         responseScenarioAnalysis.setArea(responseScenario.getArea());
         responseScenarioAnalysis.setScope(nullOrToString(responseScenario.getScope()));
@@ -85,7 +128,15 @@ public class AnalysisBuilder {
         return responseScenarioAnalysis;
     }
 
-    private ResultAnalysis buildResultAnalysis(ResponseScenario responseScenario, ElectoralList electoralList) {
+    /**
+     * Builds a result analysis for an electoral list in a response scenario.
+     *
+     * @param responseScenario The response scenario.
+     * @param electoralList    The electoral list.
+     * @return A result analysis for an electoral list in a response scenario.
+     */
+    private ResultAnalysis buildResultAnalysis(final ResponseScenario responseScenario,
+            final ElectoralList electoralList) {
         ResultAnalysis resultAnalysis = new ResultAnalysis();
         resultAnalysis.setMedian(responseScenario.getMedian(electoralList));
         Map<Integer, Float[]> confidenceIntervals = new HashMap<Integer, Float[]>();
@@ -96,7 +147,14 @@ public class AnalysisBuilder {
         return resultAnalysis;
     }
 
-    private String nullOrToString(Object object) {
+    /**
+     * Returns <code>null</code> if the provided object is <code>null</code>, or calls the <code>toString</code> on the
+     * object and returns the result of the call.
+     *
+     * @param object An object to call <code>toString</code> on if it isn't <code>null</code>.
+     * @return Either <code>null</code> or the result of calling <code>toString</code> on the object.
+     */
+    private String nullOrToString(final Object object) {
         return object == null ? null : object.toString();
     }
 }
