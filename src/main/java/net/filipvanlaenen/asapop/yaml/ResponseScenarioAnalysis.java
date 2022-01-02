41e1ed6b61e42c7a4c56b450641c7c9eee06e994
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-01-02 21:10:09 +0100

    Issue #77: Resolved Checkstyle violations for ResponseScenarioAnalysis

--- src/main/java/net/filipvanlaenen/asapop/yaml/ResponseScenarioAnalysis.java
+++ src/main/java/net/filipvanlaenen/asapop/yaml/ResponseScenarioAnalysis.java
@@ -2,41 +2,98 @@ package net.filipvanlaenen.asapop.yaml;
 
 import java.util.Map;
 
+/**
+ * Class representing the response scenario analysis element for the YAML file containing the analysis of the opinion
+ * polls in an ROPF file.
+ */
+
 public class ResponseScenarioAnalysis {
+    /**
+     * The area.
+     */
     private String area;
+    /**
+     * The analysis for the result for other.
+     */
     private ResultAnalysis otherAnalysis;
+    /**
+     * The map with the analyses for the results, with the keys of the electoral lists as their keys.
+     */
     private Map<String, ResultAnalysis> resultAnalyses;
+    /**
+     * The scope.
+     */
     private String scope;
 
+    /**
+     * Returns the area.
+     *
+     * @return The area.
+     */
     public String getArea() {
         return area;
     }
 
+    /**
+     * Returns the analysis for the result for other.
+     *
+     * @return The analysis for the result for other.
+     */
     public ResultAnalysis getOtherAnalysis() {
         return otherAnalysis;
     }
 
+    /**
+     * Returns the map with the analyses of the results.
+     *
+     * @return The map with the analyses of the result.
+     */
     public Map<String, ResultAnalysis> getResultAnalyses() {
         return resultAnalyses;
     }
 
+    /**
+     * Returns the scope.
+     *
+     * @return The scope.
+     */
     public String getScope() {
         return scope;
     }
 
-    public void setArea(String area) {
+    /**
+     * Sets the area.
+     *
+     * @param area The area.
+     */
+    public void setArea(final String area) {
         this.area = area;
     }
 
-    public void setOtherAnalysis(ResultAnalysis otherAnalysis) {
+    /**
+     * Sets the analysis for the result for other.
+     *
+     * @param otherAnalysis The analysis for the result for other.
+     */
+    public void setOtherAnalysis(final ResultAnalysis otherAnalysis) {
         this.otherAnalysis = otherAnalysis;
     }
 
-    public void setResultAnalyses(Map<String, ResultAnalysis> resultAnalyses) {
+    /**
+     * Sets the map with the analyses of the results.
+     *
+     * @param resultAnalyses The map with the analyses of the results.
+     */
+    public void setResultAnalyses(final Map<String, ResultAnalysis> resultAnalyses) {
         this.resultAnalyses = resultAnalyses;
     }
 
-    public void setScope(String scope) {
+    /**
+     * Sets the scope.
+     *
+     * @param scope The scope.
+     */
+    public void setScope(final String scope) {
         this.scope = scope;
     }
 }
