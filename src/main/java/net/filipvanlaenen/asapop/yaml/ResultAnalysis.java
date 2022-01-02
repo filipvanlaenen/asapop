3782af38ec8e49e4a75668d14cbdabf74454f56d
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-01-02 21:05:49 +0100

    Issue #77: Resolved Checkstyle violations for ResultAnalysis

--- src/main/java/net/filipvanlaenen/asapop/yaml/ResultAnalysis.java
+++ src/main/java/net/filipvanlaenen/asapop/yaml/ResultAnalysis.java
@@ -2,23 +2,53 @@ package net.filipvanlaenen.asapop.yaml;
 
 import java.util.Map;
 
+/**
+ * Class representing the result analysis element for the YAML file containing the analysis of the opinion polls in an
+ * ROPF file.
+ */
 public class ResultAnalysis {
+    /**
+     * A map with confidence intervals for the result, with their confidence levels as their keys.
+     */
     private Map<Integer, Float[]> confidenceIntervals;
+    /**
+     * The median for the result.
+     */
     private Float median;
 
+    /**
+     * Returns the map with the confidence intervals.
+     *
+     * @return The map with the confidence intervals.
+     */
     public Map<Integer, Float[]> getConfidenceIntervals() {
         return confidenceIntervals;
     }
 
+    /**
+     * Returns the median.
+     *
+     * @return The median.
+     */
     public Float getMedian() {
         return median;
     }
 
-    public void setMedian(Float median) {
-        this.median = median;
+    /**
+     * Sets the map with the confidence intervals.
+     *
+     * @param confidenceIntervals The map with the confidence intervals.
+     */
+    public void setConfidenceIntervals(final Map<Integer, Float[]> confidenceIntervals) {
+        this.confidenceIntervals = confidenceIntervals;
     }
 
-    public void setConfidenceIntervals(Map<Integer, Float[]> confidenceIntervals) {
-        this.confidenceIntervals = confidenceIntervals;
+    /**
+     * Sets the median.
+     *
+     * @param median The median.
+     */
+    public void setMedian(final Float median) {
+        this.median = median;
     }
 }
