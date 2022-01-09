21721253ab4c789a19ac27015268dec71b783901
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-01-09 23:21:50 +0100

    Issue #77: Added unit test to AnalysisBuilder

--- src/test/java/net/filipvanlaenen/asapop/yaml/AnalysisBuilderTest.java
+++ src/test/java/net/filipvanlaenen/asapop/yaml/AnalysisBuilderTest.java
@@ -3,12 +3,30 @@ package net.filipvanlaenen.asapop.yaml;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
+
+import net.filipvanlaenen.asapop.analysis.AnalysisEngine;
+import net.filipvanlaenen.asapop.model.OpinionPoll;
+import net.filipvanlaenen.asapop.model.OpinionPolls;
 
 /**
  * Unit tests on the <code>AnalysisBuilder</code> class.
  */
 public class AnalysisBuilderTest {
+    /**
+     * Sets the polling firm of an opinion poll.
+     */
+    @Test
+    public void buildingAnAnalysisShouldSetThePollingFirmOfAnOpinionPoll() {
+        OpinionPolls opinionPolls = new OpinionPolls(Set.of(new OpinionPoll.Builder().setPollingFirm("ACME").build()));
+        AnalysisEngine engine = new AnalysisEngine(opinionPolls, new ElectionData());
+        AnalysisBuilder builder = new AnalysisBuilder(engine);
+        Analysis analysis = builder.build();
+        assertEquals("ACME", analysis.getOpinionPollAnalyses().iterator().next().getPollingFirm());
+    }
+
     /**
      * <code>nullOrToString</code> should return when <code>null</code> is passed as an argument.
      */
