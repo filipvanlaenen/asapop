859068f1f0117733ca5856326dfdb1af297777e6
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-01-11 22:54:12 +0100

    Issue #77: Added unit test to AnalysisBuilder

--- src/test/java/net/filipvanlaenen/asapop/yaml/AnalysisBuilderTest.java
+++ src/test/java/net/filipvanlaenen/asapop/yaml/AnalysisBuilderTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 
 import java.util.Set;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import net.filipvanlaenen.asapop.analysis.AnalysisEngine;
@@ -16,15 +17,45 @@ import net.filipvanlaenen.asapop.model.OpinionPolls;
  */
 public class AnalysisBuilderTest {
     /**
+     * The name of the polling firm.
+     */
+    private static final String POLLING_FIRM_NAME = "ACME";
+    /**
+     * The name of the polling firm partner.
+     */
+    private static final String POLLING_FIRM_PARTNER_NAME = "BCME";
+    /**
+     * An OpinionPollAnalysis object to run the tests on.
+     */
+    private static OpinionPollAnalysis opinionPollAnalysis;
+
+    /**
+     * Initializes the test objects.
+     */
+    @BeforeAll
+    public static void createAnalysisObject() {
+        OpinionPolls opinionPolls = new OpinionPolls(Set.of(new OpinionPoll.Builder().setPollingFirm(POLLING_FIRM_NAME)
+                .setPollingFirmPartner(POLLING_FIRM_PARTNER_NAME).build()));
+        AnalysisEngine engine = new AnalysisEngine(opinionPolls, new ElectionData());
+        AnalysisBuilder builder = new AnalysisBuilder(engine);
+        Analysis analysis = builder.build();
+        opinionPollAnalysis = analysis.getOpinionPollAnalyses().iterator().next();
+    }
+
+    /**
      * Sets the polling firm of an opinion poll.
      */
     @Test
     public void buildingAnAnalysisShouldSetThePollingFirmOfAnOpinionPoll() {
-        OpinionPolls opinionPolls = new OpinionPolls(Set.of(new OpinionPoll.Builder().setPollingFirm("ACME").build()));
-        AnalysisEngine engine = new AnalysisEngine(opinionPolls, new ElectionData());
-        AnalysisBuilder builder = new AnalysisBuilder(engine);
-        Analysis analysis = builder.build();
-        assertEquals("ACME", analysis.getOpinionPollAnalyses().iterator().next().getPollingFirm());
+        assertEquals(POLLING_FIRM_NAME, opinionPollAnalysis.getPollingFirm());
+    }
+
+    /**
+     * Sets the polling firm of an opinion poll.
+     */
+    @Test
+    public void buildingAnAnalysisShouldSetThePollingFirmPartnerOfAnOpinionPoll() {
+        assertEquals(POLLING_FIRM_PARTNER_NAME, opinionPollAnalysis.getPollingFirmPartner());
     }
 
     /**
