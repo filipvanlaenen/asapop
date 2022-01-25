fce6086500282c1b59727dfa73c39e3f4f834479
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-01-25 19:47:43 +0100

    Issue #79: Started to run the calculation of a PMF for the vote shares

--- src/main/java/net/filipvanlaenen/asapop/analysis/SortableProbabilityMassFunction.java
+++ src/main/java/net/filipvanlaenen/asapop/analysis/SortableProbabilityMassFunction.java
@@ -4,16 +4,23 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.List;
 
+/**
+ * Class representing a probability mass function with sortable keys. When the keys are sortable, medians and confidence
+ * intervals can be calculated.
+ *
+ * @param <SK> The class for the sortable keys.
+ */
 abstract class SortableProbabilityMassFunction<SK> extends ProbabilityMassFunction<SK> {
+    /**
+     * Calculates the median.
+     *
+     * @return The median.
+     */
     SK getMedian() {
         BigDecimal halfProbabilityMassSum = getProbabilityMassSum().divide(new BigDecimal(2), MathContext.DECIMAL128);
-        System.out.println("HPMS: " + halfProbabilityMassSum);
         BigDecimal accumulatedProbabilityMass = BigDecimal.ZERO;
-        System.out.println("APM: " + accumulatedProbabilityMass);
         for (SK s : getSortedKeys()) {
-            System.out.println("s: " + s);
             accumulatedProbabilityMass = accumulatedProbabilityMass.add(getProbabilityMass(s), MathContext.DECIMAL128);
-            System.out.println("APM: " + accumulatedProbabilityMass);
             if (accumulatedProbabilityMass.compareTo(halfProbabilityMassSum) >= 0) {
                 return s;
             }
@@ -21,7 +28,17 @@ abstract class SortableProbabilityMassFunction<SK> extends ProbabilityMassFuncti
         return null;
     }
 
+    /**
+     * Returns the sum of the probability masses.
+     *
+     * @return The sum of the probability masses.
+     */
     abstract BigDecimal getProbabilityMassSum();
 
+    /**
+     * Returns a sorted list with the keys.
+     *
+     * @return A sorted list with the keys.
+     */
     abstract List<SK> getSortedKeys();
 }
