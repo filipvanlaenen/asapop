b29644c773f8869b63e90296504744f486683a4e
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-04-10 19:37:25 +0200

    Issue #95: Added unit tests to SampledMultivariateHypergeometricDistribution

--- src/test/java/net/filipvanlaenen/asapop/analysis/SampledMultivariateHypergeometricDistributionTest.java
+++ src/test/java/net/filipvanlaenen/asapop/analysis/SampledMultivariateHypergeometricDistributionTest.java
@@ -1,41 +1,80 @@
 package net.filipvanlaenen.asapop.analysis;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
-
-import net.filipvanlaenen.asapop.model.ElectoralList;
-import net.filipvanlaenen.asapop.model.OpinionPoll;
-import net.filipvanlaenen.asapop.model.OpinionPolls;
-import net.filipvanlaenen.asapop.model.ResultValue;
-import net.filipvanlaenen.asapop.model.Scope;
-import net.filipvanlaenen.asapop.yaml.ElectionData;
 
 /**
  * Unit tests on the <code>SampledMultivariateHypergeometricDistribution</code> class.
  */
 public class SampledMultivariateHypergeometricDistributionTest {
     /**
+     * The number of iterations in the multivariate hypergoemetric distribution.
+     */
+    private static final long NUMBER_OF_ITERATIONS = 20_000L;
+    /**
+     * The number of samples in the hypergeometric distributions.
+     */
+    private static final long NUMBER_OF_SAMPLES = 10_000L;
+    /**
+     * The sample size for the polls.
+     */
+    private static final long SAMPLE_SIZE = 1000L;
+    /**
+     * The population size of the polls.
+     */
+    private static final long POPULATION_SIZE = 1_000_000L;
+    /**
      * Precision for floating point assertions where high precision can be expected.
      */
     private static final double SMALL_DELTA = 1E-6;
     /**
+     * Precision for floating point assertions where low precision can be expected.
+     */
+    private static final double LARGE_DELTA = 0.05;
+    /**
+     * The magic number one quarter.
+     */
+    private static final double ONE_QUARTER = 0.25D;
+    /**
      * The magic number one third.
      */
     private static final double ONE_THIRD = 0.3333333D;
+    /**
+     * The magic number 0.36.
+     */
+    private static final double DOUBLE_0_36 = 0.36D;
+    /**
+     * The magic number one hundred.
+     */
+    private static final long ONE_HUNDRED = 100L;
+    /**
+     * The magic numbet two hundred ninety-nine.
+     */
+    private static final long TWO_HUNDRED_NINETY_NINE = 299L;
+    /**
+     * The magic number three hundred.
+     */
+    private static final long THREE_HUNDRED = 300L;
+    /**
+     * The magic number three hundred one.
+     */
+    private static final long THREE_HUNDRED_ONE = 301L;
+    /**
+     * The magic number five hundred.
+     */
+    private static final long FIVE_HUNDRED = 500L;
 
     /**
-     * Verifies that when there are two candidates with the same support, these candidates are certain to win the first
-     * round.
+     * Verifies that when there are two candidates with the same support, these two candidates are certain to win the
+     * first round.
      */
     @Test
     public void twoCandidatesWithTheSameSupportShouldBeCertainToWin() {
-        FirstRoundWinnersAnalysis firstRoundWinnersAnalysis = runSimulations("30", "30");
-        assertEquals(1,
-                firstRoundWinnersAnalysis.getProbabilityMass(Set.of(ElectoralList.get("L1"), ElectoralList.get("L2"))));
+        assertProbabilityEquals(1, SMALL_DELTA, 0, 1, THREE_HUNDRED, THREE_HUNDRED);
     }
 
     /**
@@ -43,44 +82,58 @@ public class SampledMultivariateHypergeometricDistributionTest {
      * probability of one third to win the first round.
      */
     @Test
-    public void threeCandidatesWithTheSameSupportShouldEachHaveAProbabilityOfOneThirdToWin() {
-        FirstRoundWinnersAnalysis firstRoundWinnersAnalysis = runSimulations("30", "30", "30");
-        assertEquals(ONE_THIRD,
-                firstRoundWinnersAnalysis.getProbabilityMass(Set.of(ElectoralList.get("L1"), ElectoralList.get("L2"))),
-                SMALL_DELTA);
+    public void threeCandidatesWithTheSameSupportShouldEachHaveAProbabilityOfOneThird() {
+        assertProbabilityEquals(ONE_THIRD, SMALL_DELTA, 0, 1, THREE_HUNDRED, THREE_HUNDRED, THREE_HUNDRED);
     }
 
     /**
      * Verifies that when there are three candidates with almost the same support, a pair of two largest candidates has
-     * a probability of more than one third to win the first round.
+     * a probability of slightly more than one third to win the first round.
      */
     @Test
-    public void twoLargestOfThreeCandidatesWithTheAlmostSameSupportShouldHaveAProbabilityAboveOneThirdToWin() {
-        FirstRoundWinnersAnalysis firstRoundWinnersAnalysis = runSimulations("31", "30", "29");
-        assertTrue(ONE_THIRD < firstRoundWinnersAnalysis
-                .getProbabilityMass(Set.of(ElectoralList.get("L1"), ElectoralList.get("L2"))));
+    public void twoLargestOfThreeCandidatesWithTheAlmostSameSupportShouldHaveAProbabilityAboveOneThird() {
+        assertProbabilityEquals(DOUBLE_0_36, LARGE_DELTA, 0, 1, THREE_HUNDRED_ONE, THREE_HUNDRED,
+                TWO_HUNDRED_NINETY_NINE);
     }
 
     /**
-     * Runs simulations based on a set of results.
-     *
-     * @param values The results to run the simulations on.
-     * @return The resulting first round winners analysis.
+     * Verifies that many small candidates do not disturb the calculation for the lagre candidates.
      */
-    private FirstRoundWinnersAnalysis runSimulations(String... values) {
-        OpinionPoll.Builder opinionPollBuilder = new OpinionPoll.Builder().setPollingFirm("ACME")
-                .setScope(Scope.PresidentialFirstRound).setFieldworkEnd("2022-03-29").setSampleSize("500");
-        int i = 0;
-        for (String value : values) {
-            opinionPollBuilder.addResult("L" + (++i), new ResultValue(value));
+    @Test
+    public void manySmallCandidatesShouldNotDisturbTheCalculationForTheLargeCandidates() {
+        assertProbabilityEquals(DOUBLE_0_36, LARGE_DELTA, 0, 1, THREE_HUNDRED_ONE, THREE_HUNDRED,
+                TWO_HUNDRED_NINETY_NINE, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L);
+    }
+
+    /**
+     * Verifies that when there are three candidates, one with half of the support, and two with equal small support,
+     * the probability that one of the smaller candidates wins the first round together with the large candidate is one
+     * quarter.
+     */
+    @Test
+    public void aCandidateWithHalfOfTheSupportTogetherWithOneOfTwoSmallCandidatesShouldHaveProbabilityOfOneQuarter() {
+        assertProbabilityEquals(ONE_QUARTER, LARGE_DELTA, 0, 1, FIVE_HUNDRED, ONE_HUNDRED, ONE_HUNDRED);
+    }
+
+    /**
+     * Asserts of a pair of candidates to be equal within a given delta.
+     *
+     * @param expected The expected probability.
+     * @param delta    The margin of error.
+     * @param i0       The index of the first candidate in the pair.
+     * @param i1       The index of the second candidate in the pair.
+     * @param values   The sample values for the candidates.
+     */
+    private void assertProbabilityEquals(double expected, double delta, int i0, int i1, long... values) {
+        List<SampledHypergeometricDistribution> probabilityMassFunctions = new ArrayList<SampledHypergeometricDistribution>();
+        for (long value : values) {
+            probabilityMassFunctions.add(
+                    SampledHypergeometricDistributions.get(value, SAMPLE_SIZE, NUMBER_OF_SAMPLES, POPULATION_SIZE));
         }
-        OpinionPoll opinionPoll = opinionPollBuilder.build();
-        OpinionPolls opinionPolls = new OpinionPolls(Set.of(opinionPoll));
-        ElectionData electionData = new ElectionData();
-        AnalysisEngine engine = new AnalysisEngine(opinionPolls, electionData);
-        engine.run(10000);
-        FirstRoundWinnersAnalysis firstRoundWinnersAnalysis = engine
-                .getFirstRoundWinnersAnalysis(opinionPoll.getMainResponseScenario());
-        return firstRoundWinnersAnalysis;
+        SampledMultivariateHypergeometricDistribution multivariateDistribution = new SampledMultivariateHypergeometricDistribution(
+                probabilityMassFunctions, POPULATION_SIZE, SAMPLE_SIZE, NUMBER_OF_ITERATIONS);
+        double actual = multivariateDistribution.getProbabilityMass(probabilityMassFunctions.get(i0),
+                probabilityMassFunctions.get(i1));
+        assertEquals(expected, actual, delta);
     }
 }
