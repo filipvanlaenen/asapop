00f89f7c730a37d6c04594d4861fbe8de86c177f
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-04-11 16:05:27 +0200

    Issue #95: Resolved Checkstyle violations in FirstRoundWinnersAnalysis

--- src/main/java/net/filipvanlaenen/asapop/analysis/FirstRoundWinnersAnalysis.java
+++ src/main/java/net/filipvanlaenen/asapop/analysis/FirstRoundWinnersAnalysis.java
@@ -1,16 +1,32 @@
 package net.filipvanlaenen.asapop.analysis;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 import net.filipvanlaenen.asapop.model.ElectoralList;
 
+/**
+ * Class representing the results of the statistical analysis of an opinion poll about the first round of a presidential
+ * election.
+ */
 public class FirstRoundWinnersAnalysis {
+    /**
+     * A map containing the probability mass of winning the first round per set of electoral lists.
+     */
     private final Map<Set<ElectoralList>, Double> probabilityMassFunction = new HashMap<Set<ElectoralList>, Double>();
 
-    FirstRoundWinnersAnalysis(VoteSharesAnalysis voteShareAnalysis,
-            SampledMultivariateHypergeometricDistribution sampledMultivariateHypergeometricDistribution) {
+    /**
+     * Constructor using a vote share analysis in combination with a sample multivariate hypergeometric distribution as
+     * its parameters.
+     *
+     * @param voteShareAnalysis                             The vote share analysis for the opinion poll.
+     * @param sampledMultivariateHypergeometricDistribution The sampled multivariate hypergeometric distribution for the
+     *                                                      opinion poll.
+     */
+    FirstRoundWinnersAnalysis(final VoteSharesAnalysis voteShareAnalysis,
+            final SampledMultivariateHypergeometricDistribution sampledMultivariateHypergeometricDistribution) {
         ElectoralList[] electoralLists = voteShareAnalysis.getElectoralLists().toArray(new ElectoralList[0]);
         for (ElectoralList electoralList : electoralLists) {
             SampledHypergeometricDistribution pmf = voteShareAnalysis.getProbabilityMassFunction(electoralList);
@@ -29,19 +45,36 @@ public class FirstRoundWinnersAnalysis {
                     probabilityMassFunction.put(Set.of(electoralLists[i], electoralLists[j]), probabilityMass);
                 }
             }
-            // TODO: Handle pairs with unknown second member.
+            double probabilityMass = sampledMultivariateHypergeometricDistribution.getProbabilityMass(pmf1, null);
+            if (probabilityMass > 0) {
+                Set<ElectoralList> key = new HashSet<ElectoralList>();
+                key.add(electoralLists[i]);
+                key.add(null);
+                probabilityMassFunction.put(key, probabilityMass);
+            }
         }
     }
 
-    public Double getProbabilityMass(Set<ElectoralList> electoralListSet) {
+    /**
+     * Returns a set with the sets of electoral lists that have a probability of winning the first round.
+     *
+     * @return A set with the sets of electoral lists that have a probability of winning the first round.
+     */
+    public Set<Set<ElectoralList>> getElectoralListSets() {
+        return probabilityMassFunction.keySet();
+    }
+
+    /**
+     * Returns the probability mass for a set of electoral lists to win the first round.
+     *
+     * @param electoralListSet A set of electoral lists.
+     * @return The probability mass to win the first round.
+     */
+    public Double getProbabilityMass(final Set<ElectoralList> electoralListSet) {
         if (probabilityMassFunction.containsKey(electoralListSet)) {
             return probabilityMassFunction.get(electoralListSet);
         } else {
             return 0D;
         }
-    }
-
-    public Set<Set<ElectoralList>> getElectoralListSets() {
-        return probabilityMassFunction.keySet();
     }
 }
