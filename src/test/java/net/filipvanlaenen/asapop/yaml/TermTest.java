b21aa4bc5542b4b68b4be76e1eb3acee49a9582e
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-09-24 18:30:51 +0200

    Issue #131: Added unit tests on Term

---
+++ src/test/java/net/filipvanlaenen/asapop/yaml/TermTest.java
@@ -0,0 +1,34 @@
+package net.filipvanlaenen.asapop.yaml;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests on the <code>Term</code> class.
+ */
+public class TermTest {
+    /**
+     * Verifies that the getter method <code>getKey</code> is wired correctly to the setter method <code>setKey</code>.
+     */
+    @Test
+    public void getKeyShouldBeWiredCorrectlyToSetKey() {
+        Term term = new Term();
+        term.setKey("foo");
+        assertEquals("foo", term.getKey());
+    }
+
+    /**
+     * Verifies that the getter method <code>getTranslations</code> is wired correctly to the setter method
+     * <code>setTranslations</code>.
+     */
+    @Test
+    public void getTranslationsShouldBeWiredCorrectlyToSetTranslations() {
+        Term term = new Term();
+        Map<String, String> translations = Map.of("en", "Foo");
+        term.setTranslations(translations);
+        assertEquals(translations, term.getTranslations());
+    }
+}
