d2534387af5ba6eda8847e1be209a1998d3e0bc9
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-12-26 16:40:38 +0100

    Issue #61: Added an abstract ExporterWarning class

---
+++ src/main/java/net/filipvanlaenen/asapop/exporter/ExporterWarning.java
@@ -0,0 +1,9 @@
+package net.filipvanlaenen.asapop.exporter;
+
+import net.filipvanlaenen.asapop.Warning;
+
+/**
+ * Abstract class defining a warning from the exporter.
+ */
+abstract class ExporterWarning implements Warning {
+}
