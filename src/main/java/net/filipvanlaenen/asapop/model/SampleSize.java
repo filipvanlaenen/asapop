dd99787356c7e80fd9e0c9f8a8d9b708ff128d1f
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-04-02 15:09:27 +0200

    Issue #210: Added sample size ranges

--- src/main/java/net/filipvanlaenen/asapop/model/SampleSize.java
+++ src/main/java/net/filipvanlaenen/asapop/model/SampleSize.java
@@ -21,9 +21,23 @@ public interface SampleSize {
         }
     }
 
+    record SampleSizeRange(int lowerBound, int upperBound) implements SampleSize {
+        public int getMinimalValue() {
+            return lowerBound;
+        }
+
+        public String getText() {
+            return Integer.toString(lowerBound) + "–" + Integer.toString(upperBound);
+        }
+    }
+
     public static SampleSize parse(String text) {
         if (text.startsWith("≥")) {
             return new MinimalSampleSize(Integer.parseInt(text.substring(1)));
+        } else if (text.contains("–")) {
+            int index = text.indexOf("–");
+            return new SampleSizeRange(Integer.parseInt(text.substring(0, index)),
+                    Integer.parseInt(text.substring(index + 1)));
         } else {
             return new ExactSampleSize(Integer.parseInt(text));
         }
