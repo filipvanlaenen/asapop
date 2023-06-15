7d1929d609c2ca4d324b4880f284a0b83d7426f5
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-06-15 15:21:45 +0200

    Issue #55: Added more functionality to the formatter

--- src/main/java/net/filipvanlaenen/asapop/exporter/RopfExporter.java
+++ src/main/java/net/filipvanlaenen/asapop/exporter/RopfExporter.java
@@ -1,5 +1,10 @@
 package net.filipvanlaenen.asapop.exporter;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import net.filipvanlaenen.asapop.model.ElectoralList;
+import net.filipvanlaenen.asapop.model.OpinionPoll;
 import net.filipvanlaenen.asapop.parser.RichOpinionPollsFile;
 
 /**
@@ -16,11 +21,62 @@ public final class RopfExporter extends Exporter {
      * Exports the rich opinion polls file.
      *
      * @param richOpinionPollsFile The rich opinion polls file to export.
-     * @return A string representing the rich opinion polls file.
+     * @return A string representing of the rich opinion polls file.
      */
     public static String export(final RichOpinionPollsFile richOpinionPollsFile) {
         StringBuffer sb = new StringBuffer();
-        sb.append("\n");
+        Set<ElectoralList> electoralLists = new HashSet<ElectoralList>();
+        for (OpinionPoll opinionPoll : richOpinionPollsFile.getOpinionPolls().getOpinionPolls()) {
+            sb.append(export(opinionPoll));
+            sb.append("\n");
+            electoralLists.addAll(getElectoralLists(opinionPoll));
+        }
+        for (ElectoralList electoralList : electoralLists) {
+            sb.append("\n");
+            sb.append(export(electoralList));
+        }
         return sb.toString();
+    }
+
+    /**
+     * Exports an electoral list.
+     *
+     * @param electoralList The electoral list to export.
+     * @return A string representation of the election list.
+     */
+    private static String export(ElectoralList electoralList) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(": ");
+        sb.append(electoralList.getId());
+        sb.append(" •A: ");
+        sb.append(electoralList.getAbbreviation());
+        return sb.toString();
+    }
+
+    /**
+     * Exports an opinion poll.
+     *
+     * @param opinionPoll The opinion poll to export.
+     * @return A string representation of the opinion poll.
+     */
+    private static String export(final OpinionPoll opinionPoll) {
+        StringBuffer sb = new StringBuffer();
+        sb.append("•PF: ");
+        sb.append(opinionPoll.getPollingFirm());
+        return sb.toString();
+    }
+
+    /**
+     * Returns a set with all the electoral lists mentioned in an opinion poll.
+     *
+     * @param opinionPoll The opinion poll.
+     * @return A set with all the electoral lists mentioned in the opinion poll.
+     */
+    private static Set<ElectoralList> getElectoralLists(final OpinionPoll opinionPoll) {
+        Set<ElectoralList> electoralLists = new HashSet<ElectoralList>();
+        for (Set<ElectoralList> electoralListCombination : opinionPoll.getElectoralListSets()) {
+            electoralLists.addAll(electoralListCombination);
+        }
+        return electoralLists;
     }
 }
