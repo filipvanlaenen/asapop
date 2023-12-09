7462ab664bb0923c7fbca18f733a7201dc031255
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-12-09 18:37:54 +0100

    Issue #253: Added a unit test on ElectoralSystem

--- src/test/java/net/filipvanlaenen/asapop/yaml/ElectoralSystemTest.java
+++ src/test/java/net/filipvanlaenen/asapop/yaml/ElectoralSystemTest.java
@@ -14,8 +14,19 @@ public class ElectoralSystemTest {
      */
     @Test
     public void getNumberOfSeatsShouldBeWiredCorrectlyToSetNumberOfSeats() {
-        ElectoralSystem slectoralSystem = new ElectoralSystem();
-        slectoralSystem.setNumberOfSeats(2);
-        assertEquals(2, slectoralSystem.getNumberOfSeats());
+        ElectoralSystem electoralSystem = new ElectoralSystem();
+        electoralSystem.setNumberOfSeats(2);
+        assertEquals(2, electoralSystem.getNumberOfSeats());
+    }
+
+    /**
+     * Verifies that the getter method <code>getThreshold</code> is wired correctly to the setter method
+     * <code>setThreshold</code>.
+     */
+    @Test
+    public void getThresholdShouldBeWiredCorrectlyToSetThreshold() {
+        ElectoralSystem electoralSystem = new ElectoralSystem();
+        electoralSystem.setThreshold(1D);
+        assertEquals(1D, electoralSystem.getThreshold());
     }
 }
