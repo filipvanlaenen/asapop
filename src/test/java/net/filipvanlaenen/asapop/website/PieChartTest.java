96148f17947b0af9d1008441fad89fa1986ad4b0
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-12-11 12:47:02 +0100

    Issue #254 and #256: Added a unit test to PieChartTest

--- src/test/java/net/filipvanlaenen/asapop/website/PieChartTest.java
+++ src/test/java/net/filipvanlaenen/asapop/website/PieChartTest.java
@@ -4,6 +4,9 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import net.filipvanlaenen.asapop.website.PieChart.Entry;
+import net.filipvanlaenen.kolektoj.Collection;
+
 /**
  * Unit tests on the <code>PieChart</code> class.
  */
@@ -20,5 +23,45 @@ public class PieChartTest {
                         + " </span>/<span id=\"pieChartTooltipDenominator\"> </span> (<span"
                         + " id=\"pieChartTooltipPercentage\"> </span>%)</div>");
         assertEquals(expected.toString(), PieChart.createTooltipDiv().asString());
+    }
+
+    /**
+     * Verifies that an empty pie chart is produced when no entries are provided.
+     */
+    @Test
+    public void shouldCreateAnEmptyPieChart() {
+        PieChart pieChart = new PieChart("pie-chart", "pie-chart-title", Collection.empty());
+        StringBuilder expected = new StringBuilder();
+        expected.append("<div class=\"pie-chart\">\n");
+        expected.append("  <svg preserveAspectRatio=\"xMinYMin meet\" viewBox=\"0 0 500 250\">\n");
+        expected.append("    <text class=\"pie-chart-title\" dominant-baseline=\"middle\" font-size=\"20\""
+                + " text-anchor=\"middle\" x=\"250\" y=\"10\"> </text>\n");
+        expected.append("  </svg>\n");
+        expected.append("</div>");
+        assertEquals(expected.toString(), pieChart.getDiv().asString());
+    }
+
+    /**
+     * Verifies that a pie chart is produced when one entry is provided.
+     */
+    @Test
+    public void shouldCreateAPieChartForOneEntry() {
+        PieChart pieChart =
+                new PieChart("pie-chart", "pie-chart-title", Collection.of(new Entry("label1", "*", 1, "slice1")));
+        StringBuilder expected = new StringBuilder();
+        expected.append("<div class=\"pie-chart\">\n");
+        expected.append("  <svg preserveAspectRatio=\"xMinYMin meet\" viewBox=\"0 0 500 250\">\n");
+        expected.append("    <text class=\"pie-chart-title\" dominant-baseline=\"middle\" font-size=\"20\""
+                + " text-anchor=\"middle\" x=\"250\" y=\"10\"> </text>\n");
+        expected.append(
+                "    <circle class=\"slice1\" cx=\"250\" cy=\"125\" onmousemove=\"showPieChartTooltip(evt, 'label1',"
+                        + " '1', '1', '100');\" onmouseout=\"hideTooltip('pieChartTooltip');\" r=\"100\"/>\n");
+        expected.append("    <text class=\"pieChartSymbol\" dominant-baseline=\"middle\" font-size=\"12\""
+                + " onmousemove=\"showPieChartTooltip(evt, 'label1', '1', '1', '100');\""
+                + " onmouseout=\"hideTooltip('pieChartTooltip');\" text-anchor=\"middle\" x=\"250\""
+                + " y=\"125\">*</text>\n");
+        expected.append("  </svg>\n");
+        expected.append("</div>");
+        assertEquals(expected.toString(), pieChart.getDiv().asString());
     }
 }
