ff866a15e9d0fe48be1608183ed6f1b3aa9b687f
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2023-12-13 11:20:47 +0100

    Issue #253: Added a unit tests on ElectionData

--- src/test/java/net/filipvanlaenen/asapop/yaml/ElectionDataTest.java
+++ src/test/java/net/filipvanlaenen/asapop/yaml/ElectionDataTest.java
@@ -9,6 +9,18 @@ import org.junit.jupiter.api.Test;
  */
 public class ElectionDataTest {
     /**
+     * Verifies that the getter method <code>getElectoralSystem</code> is wired correctly to the setter method
+     * <code>setElectoralSystem</code>.
+     */
+    @Test
+    public void getElectoralSystemShouldBeWiredCorrectlyToSetElectoralSystem() {
+        ElectionData electionData = new ElectionData();
+        ElectoralSystem electoralSystem = new ElectoralSystem();
+        electionData.setElectoralSystem(electoralSystem);
+        assertEquals(electoralSystem, electionData.getElectoralSystem());
+    }
+
+    /**
      * Verifies that the getter method <code>getPopulationSize</code> is wired correctly to the setter method
      * <code>setPopulationSize</code>.
      */
