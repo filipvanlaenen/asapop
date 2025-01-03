6d16be8ed1d77b4e56d43fd7bad44500a5e1880b
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2025-01-03 18:47:56 +0100

    Issue #270: Added the terms for the link to the iCalendar file

---
+++ src/main/java/net/filipvanlaenen/asapop/website/ICalendarFileBuilder.java
@@ -0,0 +1,77 @@
+package net.filipvanlaenen.asapop.website;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import net.filipvanlaenen.asapop.model.Election;
+import net.filipvanlaenen.asapop.model.ElectionDate;
+import net.filipvanlaenen.asapop.model.ElectionDate.ExpectedDay;
+import net.filipvanlaenen.asapop.model.ElectionDate.Qualifier;
+import net.filipvanlaenen.asapop.model.Elections;
+import net.filipvanlaenen.asapop.yaml.Terms;
+import net.filipvanlaenen.asapop.yaml.WebsiteConfiguration;
+
+public class ICalendarFileBuilder {
+    /**
+     * The elections.
+     */
+    private final Elections elections;
+    /**
+     * Today's day.
+     */
+    private final LocalDate now;
+    private final Terms terms;
+    /**
+     * The configuration for the website.
+     */
+    private final WebsiteConfiguration websiteConfiguration;
+
+    ICalendarFileBuilder(final WebsiteConfiguration websiteConfiguration, final Elections elections,
+            final LocalDate now, final Terms terms) {
+        this.websiteConfiguration = websiteConfiguration;
+        this.elections = elections;
+        this.now = now;
+        this.terms = terms;
+    }
+
+    String build() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("BEGIN:VCALENDAR\n");
+        sb.append("VERSION:2.0\n");
+        sb.append("PRODID:-//ASAPOP//");
+        sb.append(websiteConfiguration.getId());
+        sb.append("//");
+        sb.append(websiteConfiguration.getName());
+        sb.append("//EN\n");
+        List<Election> nextElections = new ArrayList<Election>(elections.getNextElections(now));
+        for (Election nextElection : nextElections) {
+            ElectionDate nextElectionDate = nextElection.getNextElectionDate(now);
+            if (nextElectionDate.qualifier().equals(Qualifier.EXACT_DATE) && nextElectionDate instanceof ExpectedDay) {
+                String isoDate = nextElectionDate.getEndDate().format(DateTimeFormatter.BASIC_ISO_DATE);
+                String areaCode = nextElection.areaCode();
+                sb.append("BEGIN:VEVENT\n");
+                sb.append("SUMMARY:🗳️");
+                sb.append(Character.toChars(127365 + areaCode.charAt(0)));
+                sb.append(Character.toChars(127365 + areaCode.charAt(1)));
+                sb.append(" Election in ");
+                sb.append(terms.getTerm("_area_" + areaCode).getTranslations().get("en"));
+                sb.append(": ");
+                sb.append(terms.getTerm(nextElection.electionType().getTermKey()).getTranslations().get("en"));
+                sb.append("\n");
+                sb.append("DTSTART:");
+                sb.append(isoDate);
+                sb.append("\n");
+                sb.append("DTEND:");
+                sb.append(isoDate);
+                sb.append("\n");
+                sb.append("END:VEVENT\n");
+            }
+        }
+        // TODO: Log a warning if no event was included.
+        sb.append("END:VCALENDAR\n");
+        return sb.toString();
+    }
+}
