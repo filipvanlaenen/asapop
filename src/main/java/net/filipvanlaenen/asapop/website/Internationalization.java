bf1d6765eeb304dc19412ddba1852a79a16840bf
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2025-01-04 14:02:03 +0100

    Issue #270: Resolved Checkstyle values

--- src/main/java/net/filipvanlaenen/asapop/website/Internationalization.java
+++ src/main/java/net/filipvanlaenen/asapop/website/Internationalization.java
@@ -8,13 +8,27 @@ import net.filipvanlaenen.asapop.yaml.Terms;
 import net.filipvanlaenen.kolektoj.ModifiableSortedMap;
 import net.filipvanlaenen.kolektoj.sortedtree.ModifiableSortedTreeMap;
 
+/**
+ * Class holding a dictionary for the internationalization.
+ */
 public class Internationalization {
+    /**
+     * A modifiable sorted map holding the dictionary.
+     */
     private ModifiableSortedMap<String, Translations> translations =
             new ModifiableSortedTreeMap<String, Translations>(Comparator.naturalOrder());
 
+    /**
+     * Default constructor.
+     */
     Internationalization() {
     }
 
+    /**
+     * Constructor taking terms as its parameters.
+     *
+     * @param terms The terms with translations.
+     */
     public Internationalization(final Terms terms) {
         for (Term term : terms.getTerms()) {
             String key = term.getKey();
@@ -22,26 +36,56 @@ public class Internationalization {
         }
     }
 
-    public void addTranslations(String key, Map<String, String> newTranslations) {
+    /**
+     * Adds translations for a key.
+     *
+     * @param key             The key.
+     * @param newTranslations A map with the translations.
+     */
+    public void addTranslations(final String key, final Map<String, String> newTranslations) {
         translations.add(key, new Translations(key, newTranslations));
     }
 
-    public boolean containsKey(String key) {
+    /**
+     * Returns whether the key is present.
+     *
+     * @param key The key.
+     * @return True if the map with the translations contains the key.
+     */
+    public boolean containsKey(final String key) {
         return translations.containsKey(key);
     }
 
-    public String getTranslation(String key, Language language) {
+    /**
+     * Returns the translation for a key and a language.
+     *
+     * @param key      The key.
+     * @param language The language.
+     * @return Returns the translation for the key in the language.
+     */
+    public String getTranslation(final String key, final Language language) {
         if (translations.containsKey(key)) {
             return translations.get(key).getTranslation(language);
         }
         return null;
     }
 
+    /**
+     * Returns all the translations as a sorted array. The array is sorted by the keys.
+     *
+     * @return A sorted array with all the translations.
+     */
     public Translations[] getTranslations() {
         return translations.getValues().toArray(new Translations[translations.size()]);
     }
 
-    public Translations getTranslations(String key) {
+    /**
+     * Returns all the translations for a key.
+     *
+     * @param key The key.
+     * @return All the translations for the key.
+     */
+    public Translations getTranslations(final String key) {
         return translations.get(key);
     }
 }
