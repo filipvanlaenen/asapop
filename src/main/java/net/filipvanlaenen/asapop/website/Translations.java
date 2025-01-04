43ea9ab892f0c630886e73ee23000c43911f9b9a
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2025-01-04 14:05:20 +0100

    Issue #270: Resolved Checkstyle values

--- src/main/java/net/filipvanlaenen/asapop/website/Translations.java
+++ src/main/java/net/filipvanlaenen/asapop/website/Translations.java
@@ -3,11 +3,26 @@ package net.filipvanlaenen.asapop.website;
 import net.filipvanlaenen.kolektoj.Map;
 import net.filipvanlaenen.kolektoj.Map.Entry;
 
+/**
+ * A class representing the translations for a key.
+ */
 class Translations {
+    /**
+     * The key.
+     */
     private final String key;
+    /**
+     * A map with the translations.
+     */
     private final Map<Language, String> translations;
 
-    Translations(String key, java.util.Map<String, String> map) {
+    /**
+     * Constructor taking the key and a map with the translations as its parameters.
+     *
+     * @param key The key.
+     * @param map A map with the translations for the key.
+     */
+    Translations(final String key, final java.util.Map<String, String> map) {
         this.key = key;
         Entry<Language, String>[] entries = new Entry[map.size()];
         int i = 0;
@@ -17,15 +32,32 @@ class Translations {
         translations = Map.of(entries);
     }
 
-    boolean containsLanguage(Language language) {
+    /**
+     * Returns whether there's a translation present for the language.
+     *
+     * @param language The language.
+     * @return True if there's a translation present for the language.
+     */
+    boolean containsLanguage(final Language language) {
         return translations.containsKey(language);
     }
 
+    /**
+     * Returns the key.
+     *
+     * @return The key.
+     */
     String getKey() {
         return key;
     }
 
-    String getTranslation(Language language) {
+    /**
+     * Returns the translation for a language.
+     *
+     * @param language The language.
+     * @return The translation for a language.
+     */
+    String getTranslation(final Language language) {
         return translations.get(language);
     }
 }
