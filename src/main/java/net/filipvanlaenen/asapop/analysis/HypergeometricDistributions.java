c751e977b451b4d4c73257f18a46444e4f4c537d
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2025-02-01 19:11:44 +0100

    Issue #304: Removed Java collections from HypergeometricDistributions

--- src/main/java/net/filipvanlaenen/asapop/analysis/HypergeometricDistributions.java
+++ src/main/java/net/filipvanlaenen/asapop/analysis/HypergeometricDistributions.java
@@ -1,18 +1,26 @@
 package net.filipvanlaenen.asapop.analysis;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import net.filipvanlaenen.kolektoj.ModifiableMap;
 
 /**
  * Class providing methods to calculate and handle hypergeometric distributions.
  */
 final class HypergeometricDistributions {
     /**
+     * A record type holding the key parameters defining a hypergeometric distribution.
+     *
+     * @param value          The value.
+     * @param sampleSize     The sample size.
+     * @param populationSize The population size.
+     */
+    private record HypergeometricDistributionKey(Long value, Long sampleSize, Long populationSize) {
+    }
+
+    /**
      * A map caching all the hypergeometric distributions created in the <code>get</code> method.
      */
-    private static final Map<List<Long>, HypergeometricDistribution> CACHE =
-            new HashMap<List<Long>, HypergeometricDistribution>();
+    private static final ModifiableMap<HypergeometricDistributionKey, HypergeometricDistribution> CACHE =
+            ModifiableMap.empty();
 
     /**
      * Private constructor to prevent the instantiation of this utility class.
@@ -29,7 +37,7 @@ final class HypergeometricDistributions {
      * @return A hypergeometric distribution.
      */
     static HypergeometricDistribution get(final Long value, final Long sampleSize, final Long populationSize) {
-        List<Long> key = List.of(value, sampleSize, populationSize);
+        HypergeometricDistributionKey key = new HypergeometricDistributionKey(value, sampleSize, populationSize);
         if (!CACHE.containsKey(key)) {
             CACHE.put(key, new HypergeometricDistribution(value, sampleSize, populationSize));
         }
