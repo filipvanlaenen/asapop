b9a70a35657723fa10467a01b0cee78ca71e4f50
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2026-01-30 21:02:49 +0100

    Bar charts for years: updated the style

--- src/main/java/net/filipvanlaenen/asapop/website/BarChart.java
+++ src/main/java/net/filipvanlaenen/asapop/website/BarChart.java
@@ -1,14 +1,14 @@
 package net.filipvanlaenen.asapop.website;
 
-import java.util.OptionalLong;
-
 import net.filipvanlaenen.kolektoj.Collection;
 import net.filipvanlaenen.kolektoj.OrderedCollection;
 import net.filipvanlaenen.kolektoj.sortedtree.SortedTreeCollection;
-import net.filipvanlaenen.tsvgj.ColorKeyword;
+import net.filipvanlaenen.tsvgj.DominantBaselineValue;
 import net.filipvanlaenen.tsvgj.Rect;
 import net.filipvanlaenen.tsvgj.Svg;
-import net.filipvanlaenen.txhtmlj.BR;
+import net.filipvanlaenen.tsvgj.Text;
+import net.filipvanlaenen.tsvgj.TextAnchorValue;
+import net.filipvanlaenen.tsvgj.Transform;
 import net.filipvanlaenen.txhtmlj.Div;
 import net.filipvanlaenen.txhtmlj.FlowContent;
 import net.filipvanlaenen.txhtmlj.Span;
@@ -17,6 +17,10 @@ import net.filipvanlaenen.txhtmlj.Span;
  * Class building bar charts in SVG.
  */
 class BarChart extends Chart {
+    /**
+     * The ID for the HTML element containing an entry's label.
+     */
+    private static final String LABEL_CLASS = "barChartLabel";
     /**
      * The ID for the HTML element containing the bar chart tooltip.
      */
@@ -37,11 +41,11 @@ class BarChart extends Chart {
     /**
      * Record type to contain entries for a pie chart.
      *
-     * @param labelClass The class for this entry's label.
+     * @param label      The class for this entry's label.
      * @param value      The value for this entry.
      * @param sliceClass The class for this entry's slice in the pie chart.
      */
-    record Entry(String labelClass, long value, String sliceClass) implements Chart.Entry {
+    record Entry(String label, long value, String sliceClass) implements Chart.Entry {
     }
 
     /**
@@ -83,6 +87,7 @@ class BarChart extends Chart {
         int i = 0;
         for (Entry entry : entries) {
             long value = entry.value();
+            String label = entry.label();
             double x = LEFT_X + slotWidth * 0.05D + i * slotWidth;
             double height = CHART_CANVAS_HEIGHT * value / maximumValue;
             String sliceClass = entry.sliceClass();
@@ -90,10 +95,18 @@ class BarChart extends Chart {
                 sliceClass = "bar-chart-" + (i % TWELVE + 1);
             }
             Rect rect = new Rect().x(x).y(BOTTOM_Y - height).height(height).width(barWidth).clazz(sliceClass);
-            String onMouseMoveEvent = "showBarChartTooltip(evt, '" + entry.labelClass() + "', '" + value + "');";
+            String onMouseMoveEvent = "showBarChartTooltip(evt, '" + label + "', '" + value + "');";
             String onMouseOutEvent = "hideTooltip('" + TOOLTIP_ID + "');";
             rect.onmousemove(onMouseMoveEvent).onmouseout(onMouseOutEvent);
             svg.addElement(rect);
+            double labelX = LEFT_X + slotWidth * 0.5D + i * slotWidth;
+            double labelFontSize = barWidth > TITLE_HEIGHT ? TITLE_HEIGHT : barWidth * 0.9D;
+            double labelY = BOTTOM_Y - labelFontSize * 0.2D;
+            Text symbol = new Text(label).x(labelX).y(labelY).fontSize(labelFontSize).textAnchor(TextAnchorValue.START)
+                    .dominantBaseline(DominantBaselineValue.MIDDLE).transform(Transform.rotate(-90, labelX, labelY))
+                    .clazz(LABEL_CLASS).onmousemove(onMouseMoveEvent).onmouseout(onMouseOutEvent);
+            svg.addElement(symbol);
+
             i++;
         }
     }
