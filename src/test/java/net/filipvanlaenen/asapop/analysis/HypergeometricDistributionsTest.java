268e8b28429a903095f99c37ced668e683bb3dcf
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2026-02-10 16:46:52 +0100

    Added a unit test to HypergeometricDistributionsTest

--- src/test/java/net/filipvanlaenen/asapop/analysis/HypergeometricDistributionsTest.java
+++ src/test/java/net/filipvanlaenen/asapop/analysis/HypergeometricDistributionsTest.java
@@ -3,6 +3,8 @@ package net.filipvanlaenen.asapop.analysis;
 import static org.junit.Assert.assertSame;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.time.Instant;
+
 import org.junit.jupiter.api.Test;
 
 /**
@@ -17,6 +19,10 @@ public class HypergeometricDistributionsTest {
      * The magic number five.
      */
     private static final long FIVE = 5L;
+    /**
+     * The magic number 997.
+     */
+    private static final long L997 = 997L;
 
     /**
      * Verifies that it retrieves a hypergeometric distribution that is equal to the correct hypergeometric
@@ -33,5 +39,15 @@ public class HypergeometricDistributionsTest {
     @Test
     public void shouldRetrieveTheSameObjectWhenAskingForTheSameHypergeometricDistribution() {
         assertSame(HypergeometricDistributions.get(1L, FOUR, FIVE), HypergeometricDistributions.get(1L, FOUR, FIVE));
+    }
+
+    /**
+     * Verifies that it calculates a hypergeometric distribution if it hasn't been retrieved yet.
+     */
+    @Test
+    public void shouldRetrieveANewHypergeometricDistribution() {
+        long populationSize = Instant.now().toEpochMilli() % L997;
+        assertEquals(new HypergeometricDistribution(1L, FOUR, populationSize),
+                HypergeometricDistributions.get(1L, FOUR, populationSize));
     }
 }
