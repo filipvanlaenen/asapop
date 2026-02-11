34f10162d8d040125f2e04ff0fc7371a58aab6ba
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2026-02-11 21:30:53 +0100

    Added grid lines to the bar charts

--- src/main/java/net/filipvanlaenen/asapop/website/BarChart.java
+++ src/main/java/net/filipvanlaenen/asapop/website/BarChart.java
@@ -1,9 +1,13 @@
 package net.filipvanlaenen.asapop.website;
 
+import java.util.Comparator;
+
 import net.filipvanlaenen.kolektoj.Collection;
 import net.filipvanlaenen.kolektoj.OrderedCollection;
 import net.filipvanlaenen.kolektoj.sortedtree.SortedTreeCollection;
+import net.filipvanlaenen.nombrajkolektoj.longs.SortedLongCollection;
 import net.filipvanlaenen.tsvgj.DominantBaselineValue;
+import net.filipvanlaenen.tsvgj.Line;
 import net.filipvanlaenen.tsvgj.Rect;
 import net.filipvanlaenen.tsvgj.Svg;
 import net.filipvanlaenen.tsvgj.Text;
@@ -17,6 +21,11 @@ import net.filipvanlaenen.txhtmlj.Span;
  * Class building bar charts in SVG.
  */
 class BarChart extends Chart {
+    private static final SortedLongCollection MAJOR_GRID_STEPS =
+            SortedLongCollection.of(Comparator.naturalOrder(), 1L, 2L, 5L, 10L, 20L, 50L, 100L, 200L, 500L, 1000L,
+                    2000L, 5000L, 10000L, 20000L, 50000L, 100000L, 200000L, 500000L, 1000000L, 2000000L, 5000000L);
+    private static final SortedLongCollection MINOR_GRID_STEPS = SortedLongCollection.of(Comparator.naturalOrder(), 1L,
+            5L, 10L, 50L, 100L, 500L, 1000L, 5000L, 10000L, 50000L, 100000L, 500000L, 1000000L, 5000000L);
     /**
      * The ID for the HTML element containing an entry's label.
      */
@@ -37,6 +46,8 @@ class BarChart extends Chart {
      * The magic number twelve.
      */
     private static final int TWELVE = 12;
+    private static final String MAJOR_GRID_LINE_CLASS = "major-grid-line";
+    private static final String MINOR_GRID_LINE_CLASS = "minor-grid-line";
 
     /**
      * Record type to contain entries for a pie chart.
@@ -84,6 +95,25 @@ class BarChart extends Chart {
         double slotWidth = CHART_CANVAS_WIDTH / n;
         double barWidth = slotWidth * 0.9D;
         long maximumValue = entries.stream().mapToLong(Entry::value).max().getAsLong();
+        long gridValue = 0L;
+        long majorGridStep = MAJOR_GRID_STEPS.getGreaterThanOrEqualTo(maximumValue / 5L);
+        long minorGridStep = majorGridStep > 1L ? MINOR_GRID_STEPS.getLessThan(majorGridStep) : 1L;
+        while (gridValue <= maximumValue) {
+            if (gridValue % majorGridStep != 0) {
+                double y = BOTTOM_Y - CHART_CANVAS_HEIGHT * gridValue / maximumValue;
+                Line line = new Line().x1(LEFT_X).x2(RIGHT_X).y1(y).y2(y).strokeWidth(0.2D);
+                line.clazz(MINOR_GRID_LINE_CLASS);
+                svg.addElement(line);
+            }
+            gridValue += minorGridStep;
+        }
+        gridValue = 0L;
+        while (gridValue <= maximumValue) {
+            double y = BOTTOM_Y - CHART_CANVAS_HEIGHT * gridValue / maximumValue;
+            Line line = new Line().x1(LEFT_X).x2(RIGHT_X).y1(y).y2(y).strokeWidth(0.2D).clazz(MAJOR_GRID_LINE_CLASS);
+            svg.addElement(line);
+            gridValue += majorGridStep;
+        }
         int i = 0;
         for (Entry entry : entries) {
             long value = entry.value();
