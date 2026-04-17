32cdeeb1ab4945bd5a1365e91f4b019015458f15
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2026-04-17 15:54:06 +0200

    Resolved Checkstyle violations in Area

--- src/main/java/net/filipvanlaenen/asapop/model/Area.java
+++ src/main/java/net/filipvanlaenen/asapop/model/Area.java
@@ -3,20 +3,54 @@ package net.filipvanlaenen.asapop.model;
 import net.filipvanlaenen.kolektoj.Collection;
 import net.filipvanlaenen.kolektoj.ModifiableMap;
 
+/**
+ * Class representing an area.
+ */
 public class Area {
-    private static final ModifiableMap<String, Area> instances = ModifiableMap.<String, Area>empty();
+    /**
+     * A map with all the instances.
+     */
+    private static final ModifiableMap<String, Area> INSTANCES = ModifiableMap.<String, Area>empty();
+
+    /**
+     * Adds an area to the map of instances.
+     *
+     * @param area The area to be added.
+     */
+    public static void add(final Area area) {
+        INSTANCES.add(area.getId(), area);
+    }
+
+    /**
+     * Returns all instances.
+     *
+     * @return A collection with all the instances.
+     */
+    public static Collection<Area> getAll() {
+        return INSTANCES.getValues();
+    }
+
+    /**
+     * The ID.
+     */
     private final String id;
+    /**
+     * The elected bodies of the area.
+     */
     private final Collection<ElectedBody> electedBodies;
+    /**
+     * The elected offices of the aread.
+     */
     private final Collection<ElectedOffice> electedOffices;
 
-    public static void add(Area area) {
-        instances.add(area.getId(), area);
-    }
-
-    public static Collection<Area> getAll() {
-        return instances.getValues();
-    }
-
+    /**
+     * Constructor taking an ID, a collection with the elected bodies and a collection with the elected offices as its
+     * parameters.
+     *
+     * @param id             The ID.
+     * @param electedBodies  The elected bodies.
+     * @param electedOffices The elected offices.
+     */
     public Area(final String id, final Collection<ElectedBody> electedBodies,
             final Collection<ElectedOffice> electedOffices) {
         this.id = id;
@@ -24,14 +58,29 @@ public class Area {
         this.electedOffices = Collection.of(electedOffices);
     }
 
+    /**
+     * Returns the elected bodies.
+     *
+     * @return A collection with the elected bodies.
+     */
     public Collection<ElectedBody> getElectedBodies() {
         return electedBodies;
     }
 
+    /**
+     * Returns the elected offices.
+     *
+     * @return A collection with the elected offices.
+     */
     public Collection<ElectedOffice> getElectedOffices() {
         return electedOffices;
     }
 
+    /**
+     * Returns the ID.
+     *
+     * @return The ID.
+     */
     public String getId() {
         return id;
     }
