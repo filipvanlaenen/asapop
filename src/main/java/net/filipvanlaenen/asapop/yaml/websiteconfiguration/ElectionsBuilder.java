843f6842147c66709b58c40f265205dc49af439d
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2026-04-17 17:45:37 +0200

    Removing unneeded functionality

--- src/main/java/net/filipvanlaenen/asapop/yaml/websiteconfiguration/ElectionsBuilder.java
+++ src/main/java/net/filipvanlaenen/asapop/yaml/websiteconfiguration/ElectionsBuilder.java
@@ -31,18 +31,12 @@ public final class ElectionsBuilder {
      * @param electionDataFiles
      */
     private static void addElectionList(final Elections elections, final ElectionList electionList,
-            final String areaCode, final ElectionType electionType, final Map<String, ElectionData> electionDataFiles,
-            final LocalDate now, final Token token) {
+            final String areaCode, final ElectionType electionType, final Map<String, ElectionData> electionDataFiles) {
         if (electionList != null) {
-            Token electionTypeToken = Laconic.LOGGER.logMessage(token,
-                    "Extracting and validating the election dates for election type %s.", electionType.getTermKey());
             for (Map.Entry<Integer, String> entry : electionList.getDates()) {
                 int electionNumber = entry.key();
                 ElectionData electionData = electionDataFiles.get(areaCode + "-" + electionNumber, null);
                 elections.addElection(areaCode, electionType, electionNumber, entry.value(), electionData);
-            }
-            if (elections.getNextElection(areaCode, electionType, now) == null) {
-                Laconic.LOGGER.logError("No election dates set in the future.", electionTypeToken);
             }
         }
     }
@@ -57,7 +51,6 @@ public final class ElectionsBuilder {
     public static Elections extractAndValidateElections(final WebsiteConfiguration websiteConfiguration,
             final Map<String, ElectionData> electionDataFiles, final LocalDate now) {
         Token token = Laconic.LOGGER.logMessage("Extracting and validating the election dates.");
-        Laconic.LOGGER.logMessage("Validating for future election dates against the date %s.", now.toString(), token);
         Elections elections = new Elections();
         Set<AreaConfiguration> areaConfigurations = websiteConfiguration.getAreaConfigurations();
         if (areaConfigurations == null) {
@@ -69,12 +62,13 @@ public final class ElectionsBuilder {
                     "Extracting and validating the election dates for area %s.", areaCode);
             ElectionLists electionLists = areaConfiguration.getElections();
             if (electionLists != null) {
+                Laconic.LOGGER.logError("The area contains the deprecated elections field.", areaToken);
                 addElectionList(elections, electionLists.getNational(), areaCode, ElectionType.NATIONAL,
-                        electionDataFiles, now, areaToken);
+                        electionDataFiles);
                 addElectionList(elections, electionLists.getPresidential(), areaCode, ElectionType.PRESIDENTIAL,
-                        electionDataFiles, now, areaToken);
+                        electionDataFiles);
                 addElectionList(elections, electionLists.getEuropean(), areaCode, ElectionType.EUROPEAN,
-                        electionDataFiles, now, areaToken);
+                        electionDataFiles);
             }
         }
         return elections;
