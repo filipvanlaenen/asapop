ab2feed0a72a4577bd6f05d2a99e6d0b7bc96cff
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2026-05-19 14:21:14 +0200

    Added unit tests on WebsiteConfiguration

--- src/test/java/net/filipvanlaenen/asapop/yaml/websiteconfiguration/WebsiteConfigurationTest.java
+++ src/test/java/net/filipvanlaenen/asapop/yaml/websiteconfiguration/WebsiteConfigurationTest.java
@@ -11,6 +11,16 @@ import org.junit.jupiter.api.Test;
  */
 public class WebsiteConfigurationTest {
     /**
+     * Verifies that the getter method <code>getId</code> is wired correctly to the setter method <code>setId</code>.
+     */
+    @Test
+    public void getIdShouldBeWiredCorrectlyToSetId() {
+        WebsiteConfiguration websiteConfiguration = new WebsiteConfiguration();
+        websiteConfiguration.setId("ab");
+        assertEquals("ab", websiteConfiguration.getId());
+    }
+
+    /**
      * Verifies that the getter method <code>getAreaConfigurations</code> is wired correctly to the setter method
      * <code>setAreaConfigurations</code>.
      */
@@ -20,5 +30,28 @@ public class WebsiteConfigurationTest {
         Set<AreaConfiguration> areaConfigurations = Set.of(new AreaConfiguration(), new AreaConfiguration());
         websiteConfiguration.setAreaConfigurations(areaConfigurations);
         assertEquals(areaConfigurations, websiteConfiguration.getAreaConfigurations());
+    }
+
+    /**
+     * Verifies that the getter method <code>getName</code> is wired correctly to the setter method
+     * <code>setName</code>.
+     */
+    @Test
+    public void getNameShouldBeWiredCorrectlyToSetName() {
+        WebsiteConfiguration websiteConfiguration = new WebsiteConfiguration();
+        websiteConfiguration.setName("Foo");
+        assertEquals("Foo", websiteConfiguration.getName());
+    }
+
+    /**
+     * Verifies that the getter method <code>getWidgetsConfigurations</code> is wired correctly to the setter method
+     * <code>setWidgetsConfigurations</code>.
+     */
+    @Test
+    public void getWidgetsConfigurationsShouldBeWiredCorrectlyToSetWidgetsConfigurations() {
+        WebsiteConfiguration websiteConfiguration = new WebsiteConfiguration();
+        WidgetsConfiguration widgetsConfiguration = new WidgetsConfiguration();
+        websiteConfiguration.setWidgetsConfiguration(widgetsConfiguration);
+        assertEquals(widgetsConfiguration, websiteConfiguration.getWidgetsConfiguration());
     }
 }
